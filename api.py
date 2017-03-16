ef7d96af003398caa9b28f09710c3d50ff74b88a
Chris Li <chrisshwli@gmail.com>
2017-03-16 10:21:21 -0400

    update host

--- api.py
+++ api.py
@@ -21,4 +21,6 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='127.0.0.0')
+    application.run(host='0.0.0.0')
+
+    
