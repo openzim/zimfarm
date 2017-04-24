4366fd3776655b5083b6f15a1f31720263d364ab
Chris Li <chrisshwli@gmail.com>
2017-04-24 16:44:00 -0400

    Adjust tabs

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'zimfarm',
-  templateUrl: 'app/app.component.html'
+    selector: 'zimfarm',
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+    
+
+}
