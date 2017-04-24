b1f38815e58c1ed72ce2dfac7662a33c36d73295
Chris Li <chrisshwli@gmail.com>
2017-04-24 16:39:10 -0400

    Add nav bar

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'zimfarm',
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent  { name = 'Angular'; }
