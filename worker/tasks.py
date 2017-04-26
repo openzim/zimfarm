820fd32d5a13a8b20dbc0dcd6b2a9cea902f4511
Chris Li <chrisshwli@gmail.com>
2017-04-26 11:17:14 -0400

    Simple worker configuration

---
+++ worker/tasks.py
@@ -0,0 +1,13 @@
+from celery import Celery
+import time
+
+
+app = Celery('worker', broker='amqp://admin:mypass@rabbit:5672', backend='rpc://')
+
+
+@app.task
+def longtime_add(x, y):
+	print('long time task begins')
+	time.sleep(5)
+	print('long time task finished')
+	return x + y
