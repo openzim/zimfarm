71dc4f751bcec30c813bc5e1f82f31475079ac2b
Chris Li <chrisshwli@gmail.com>
2017-04-26 15:22:19 -0400

    add a simple task to worker

--- dispatcher/app/main.py
+++ dispatcher/app/main.py
@@ -1,9 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+from celery import Celery
 
-@app.route("/")
+flask_app = Flask(__name__)
+celery_app = Celery('worker', broker='amqp://admin:mypass@rabbit:5672', backend='rpc://')
+
+@flask_app.route("/")
 def hello():
-   return "Hello World from Flask"
+   return "Hello World [ccust] from Flask using Python 3.5"
+
+@flask_app.route("/test")
+def test():
+    task = celery_app.send_task('test.add', args=[4, 7])
+    response = "<a>check status of {id} </a>".format(id=task.id)
+    return response
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', debug=True, port=80)
+   flask_app.run(host='0.0.0.0', debug=True, port=80)
