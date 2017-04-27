46029dd9edd512e26885c8a2f5e3e7ab71bf4e58
Chris Li <chrisshwli@gmail.com>
2017-04-27 09:44:04 -0400

    Delayed add test app

--- dispatcher/app/main.py
+++ dispatcher/app/main.py
@@ -1,19 +1,41 @@
-from flask import Flask, jsonify
-from celery import Celery
+from flask import Flask, request, jsonify
+import celery
+
 
 flask_app = Flask(__name__)
-celery_app = Celery('worker', broker='amqp://admin:mypass@rabbit:5672', backend='rpc://')
+celery_app = celery.Celery('worker', broker='amqp://admin:mypass@rabbit:5672', backend='rpc://')
+
 
 @flask_app.route("/")
 def hello():
-   return "Hello World [ccust] from Flask using Python 3.5"
+   return "Hello World from zim farm using Python 3.5"
 
-@flask_app.route("/test")
-def test():
-    task = celery_app.send_task('delayed_add', args=[4, 7])
+@flask_app.route("/task/delayed_add", methods=['POST'])
+def task_delayed_add():
+    request_json = request.get_json()
+    x = request_json['x']
+    y = request_json['y']
+    name = 'delayed_add'
+    task = celery_app.send_task(name, args=[x, y])
     response = {
-        'task_id': task.id
+        'task': {
+            'id': task.id,
+            'name': name,
+            'status': task.status
+        },
     }
+    return jsonify(response)
+
+@flask_app.route("/task/status/<string:id>")
+def task_status(id):
+    res = celery_app.AsyncResult(id)
+    status = res.status
+    response = {
+        'id': id,
+        'status': status,
+    }
+    if status == celery.states.SUCCESS:
+        response['result'] = res.result
     return jsonify(response)
 
 if __name__ == "__main__":
--- worker/app/tasks.py
+++ worker/app/tasks.py
@@ -5,10 +5,10 @@ import time
 app = Celery('worker', broker='amqp://admin:mypass@rabbit:5672', backend='rpc://')
 
 
-@app.task(name='delayed_add')
+@app.task(name='delayed_add', track_started=True)
 def delayed_add(x, y):
-	print('delayed add begins: {} + {} = ??'.format(x, y))
-	time.sleep(5)
-	result = x + y
-	print('delayed add finished: {} + {} = {}'.format(x, y, result))
-	return result
+    print('delayed add begins: {} + {} = ??'.format(x, y))
+    time.sleep(5)
+    result = x + y
+    print('delayed add finished: {} + {} = {}'.format(x, y, result))
+    return result
