0b6ab7bbf8076325cc8234975138bc91f8f84565
Chris Li <chrisshwli@gmail.com>
2017-05-25 14:10:05 -0400

    Add old cards back

--- dispatcher_frontend/src/app/dashboard/dashboard.module.ts
+++ dispatcher_frontend/src/app/dashboard/dashboard.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard.component';
 // import { TaskProgressComponent } from './tasks-progress.component';
@@ -6,7 +7,8 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-    imports: [DashboardRoutingModule],
+    imports: [CommonModule,
+              DashboardRoutingModule],
     declarations: [DashboardComponent]
 })
 export class DashboardModule { }
