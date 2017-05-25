8cb6f9d5a4652eb6f742b93058e5e2b21ad2a971
Chris Li <chrisshwli@gmail.com>
2017-05-25 14:47:17 -0400

    Draw a dimple dounut chart

--- dispatcher_frontend/src/app/dashboard/task-progress/tasks-progress.component.ts
+++ dispatcher_frontend/src/app/dashboard/task-progress/tasks-progress.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Chart, LinearChartData } from 'chart.js';
 
 @Component({
     selector: 'task-progress',
     templateUrl: 'app/dashboard/task-progress/tasks-progress.component.html'
 })
-export class TaskProgressComponent {
+export class TaskProgressComponent implements OnInit {
+    @ViewChild('taskProgress') canvas: ElementRef;
+    chart: Chart;
+    
+    ngOnInit() {
+        this.configureChart();
+        this.chart.update();
+    }
+
+    configureChart(): void {
+        let context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext("2d");
+        let data = {
+            labels: [
+                "Completed",
+                "Executing",
+                "Scheduled"
+            ],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+        };
+        let options = {
+
+        }
+        this.chart = new Chart(context, {type: 'doughnut', data: data});
+    }
 }
