5a77febdd455bde2208b1d1ae32f4936c2f81e96
Chris Li <chrisshwli@gmail.com>
2017-06-10 22:32:32 -0400

    show tabs conditionally

--- dispatcher_frontend/src/app/navigation-bar/navigation-bar.component.ts
+++ dispatcher_frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription }   from 'rxjs/Subscription';
 
 import { AuthService } from '../service/auth.service';
 
@@ -7,17 +8,28 @@ import { AuthService } from '../service/auth.service';
     templateUrl: 'app/navigation-bar/navigation-bar.component.html'
 })
 export class NavigationBarComponent {
+    private scopeSubscription: Subscription;
+    tabs: any[] = [];
 
-    constructor(
-        private authService: AuthService
-    ){}
+    constructor(private authService: AuthService) {
+        this.configureTabs(authService.scope);
+        this.scopeSubscription = authService.scopeChange.subscribe(scope => {
+            this.configureTabs(scope);
+        })
+    }
 
-    tabs = [
-        {name: "Dashboard", routerLink: "dashboard"},
-        {name: "Tasks", routerLink: "tasks"},
-        {name: "Nodes", routerLink: "nodes"}
-    ]
+    configureTabs(scopes: string[]) {
+        this.tabs = [];
+        for (let scope of scopes) {
+            if (scope == 'dashboard') {
+                this.tabs.push({name: "Dashboard", routerLink: "dashboard"});
+            } else if (scope == 'tasks') {
+                this.tabs.push({name: "Tasks", routerLink: "tasks"});
+            }
+        }
+    }
 
-    ngOnInit() {
+    ngOnDestroy() {
+        this.scopeSubscription.unsubscribe();
     }
 }
--- dispatcher_frontend/src/app/service/auth.service.ts
+++ dispatcher_frontend/src/app/service/auth.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Subject }    from 'rxjs/Subject';
+import { JwtHelper } from 'angular2-jwt';
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class AuthService {
-    constructor(private http: Http) { }
+    private jwt: JwtHelper = new JwtHelper();
+    scope: string[] = [];
+    scopeChange: Subject<string[]> = new Subject<string[]>();
+
+    constructor(private http: Http) {
+        let token = this.getToken();
+        this.decodeScope(token);
+    }
 
     login(username: string, password: string) {
         const url = 'api/auth/login';
         let body = JSON.stringify({username: username, password: password});
         let options = new RequestOptions ({
-            headers: new Headers({'Content-Type': 'application/json'});
+            headers: new Headers({'Content-Type': 'application/json'})
         })
         return this.http.post('api/auth/login', body, options)
             .toPromise()
             .then(response => {
                 let json  = response.json();
                 if (json.success) {
-                    localStorage.setItem('token', json.token);
+                    this.setToken(json.token);
+                    this.decodeScope(json.token);
                 }
                 return json.success
             })
@@ -31,7 +40,21 @@ export class AuthService {
         return Promise.reject(error.message || error);
     }
 
+    private decodeScope(token?: string) {
+        this.scope = token == null ? [] : this.scope = this.jwt.decodeToken(token).scope;
+        this.scopeChange.next(this.scope);
+    }
+
+    private getToken(): string {
+        return localStorage.getItem('token');
+    }
+
+    private setToken(token: string) {
+        localStorage.setItem('token', token);
+    }
+
     logout() {
         localStorage.removeItem('token');
+        this.decodeScope(null);
     }
 }
