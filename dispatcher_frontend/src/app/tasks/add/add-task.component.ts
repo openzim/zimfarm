d52bc6fcf3e00d9137e588e97dbdb4070296f897
Chris Li <chrisshwli@gmail.com>
2017-06-14 11:42:26 -0400

    add task

--- dispatcher_frontend/src/app/tasks/add/add-task.component.ts
+++ dispatcher_frontend/src/app/tasks/add/add-task.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'tasks',
-    templateUrl: './add-task.component.html'
+    templateUrl: './add-task.component.html',
+    styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
 
