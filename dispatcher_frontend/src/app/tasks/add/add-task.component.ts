bb5041550374dbcc948726274f1f58b82bc370c3
Chris Li <chrisshwli@gmail.com>
2017-06-16 14:29:22 -0400

    add task layout

--- dispatcher_frontend/src/app/tasks/add/add-task.component.ts
+++ dispatcher_frontend/src/app/tasks/add/add-task.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'tasks',
     templateUrl: './add-task.component.html',
     styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
+    taskAddingMode: string
+
+    ngOnInit() {
+        this.taskAddingMode = "script";
+    }
+
+    didSelectModeTab(mode: string) {
+        this.taskAddingMode = mode;
+    }
 
 }
