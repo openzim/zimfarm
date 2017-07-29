5cfd8255559864c08c4dcbe0865de50263441ab6
Chris Li <chrisshwli@gmail.com>
2017-07-29 11:49:24 -0400

    mwoffliner worker update

--- worker/app/tasks.py
+++ worker/app/tasks.py
@@ -29,16 +29,26 @@ class Generic(Task):
 class MWOffliner(Task):
     name = 'zimfarm.mwoffliner'
 
-    def run(self):
+    def run(self, mw_url: str, admin_email: str, **kwargs):
+        redis_container_name = 'zimfarm-worker-redis'
+
         def run_redis():
-            redis_container_name = 'zimfarm-worker-redis'
             containers = client.containers.list(filters={'name': redis_container_name})
             if len(containers) == 0:
                 client.containers.run('redis', detach=True, name=redis_container_name)
 
         try:
             client = docker.from_env()
+
             run_redis()
+            logger.info('MWOffliner: redis started')
+
+            command = ['mwoffliner', '--redis="redis://redis"',
+                       '--mwUrl={}'.format(mw_url), '--adminEmail={}'.format(admin_email)]
+            log = client.containers.run('openzim/mwoffliner', command, name=self.request.id,
+                                        links={redis_container_name: 'redis'})
+            logger.info('MWOffliner: {}'.format(log.decode()))
+
         except docker.errors.ContainerError as e:
             logger.error('DOCKER: ContainerError({})'.format(e.stderr))
         except docker.errors.ImageNotFound as e:
