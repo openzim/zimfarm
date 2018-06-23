dc14a9250ca70786bab0d579c71eac8ffddc5756
Chris Li <chrisshwli@gmail.com>
2018-06-23 08:03:57 -0400

    schedule

--- dispatcher/frontend/src/app/components/schedule/schedule.component.ts
+++ dispatcher/frontend/src/app/components/schedule/schedule.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { SchedulesService } from '../../services/schedules.service';
+import { SchedulesService, SchedulesListResponseData, SchedulesListMeta, Schedule } from '../../services/schedules.service';
 
 @Component({
     templateUrl: './schedule.component.html',
+    styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
     constructor(private router: Router, private schedulesService: SchedulesService) {}
 
+    private schedules: Array<Schedule> = []
+    private meta: SchedulesListMeta;
+
     ngOnInit() {
         this.schedulesService.list().subscribe(data => {
-            console.log(data)
+            this.schedules = data.items
+            this.meta = data.meta
+        })
+    }
+
+    goPrevious(): void {
+        let skip = Math.max(0, this.meta.skip - 20);
+        this.schedulesService.list(skip, 20).subscribe(data => {
+            this.schedules = data.items
+            this.meta = data.meta
+        })
+    }
+
+    goNext(): void {
+        this.schedulesService.list(this.meta.skip + 20, 20).subscribe(data => {
+            this.schedules = data.items
+            this.meta = data.meta
         })
     }
 }
--- dispatcher/frontend/src/app/services/schedules.service.ts
+++ dispatcher/frontend/src/app/services/schedules.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpInterceptor, HttpHeaders, HttpRequest, HttpHandler, Htt
 import { Observable, throwError } from 'rxjs';
 
 import { apiRoot } from './config';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -10,26 +11,54 @@ import { apiRoot } from './config';
 export class SchedulesService {
     constructor(private http: HttpClient) { }
 
-    list(offset: number = 0, limit: number = 20) {
-        return this.http.get<SchedulesListResponseData>('https://farm.openzim.org/api/schedules/')
+    list(skip: number = 0, limit: number = 20) {
+        return this.http.get<SchedulesListResponseData>(
+            'https://farm.openzim.org/api/schedules/', 
+            {
+                params: {
+                    skip: skip.toString(),
+                    limit: limit.toString()
+                }
+            }
+        ).pipe(map(data => {
+            console.log(data.items);
+            for (let item of data.items) {
+                item.beat = new Beat(item.beat.type, item.beat.config);
+            }
+            return data
+        }))
     }
 }
 
-interface SchedulesListResponseData {
+export interface SchedulesListResponseData {
     items: Array<Schedule>;
     meta: SchedulesListMeta;
 }
 
-interface SchedulesListMeta {
+export interface SchedulesListMeta {
     limit: number;
     skip: number;
 }
 
-interface Schedule {
+export interface Schedule {
     _id: string;
     category: string;
     language: string;
     offliner: string;
-    schedule: Object;
+    beat: Beat;
     task: Object;
+}
+
+export class Beat {
+    type: string;
+    config: Object;
+
+    constructor(type: string, config: Object) {
+        this.type = type
+        this.config = config
+    }
+
+    description(): string {
+        return `${this.type}(day_of_month=${this.config['day_of_month']})`
+    }
 }
