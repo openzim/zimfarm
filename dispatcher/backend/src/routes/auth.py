c066190014461e9ce9430a5c47facf5472a1dd6a
Chris Li <chrisshwli@gmail.com>
2018-06-28 14:09:52 -0400

    rabbit auth backend

--- dispatcher/backend/src/routes/auth.py
+++ dispatcher/backend/src/routes/auth.py
@@ -140,36 +140,35 @@ def rabbitmq_user(intention: str):
     """
     Handles RabbitMQ auth http backend request
     See also: https://github.com/rabbitmq/rabbitmq-auth-backend-http
-
-    :param intention: the sub-path of the request
-    :return Response: allow or deny the auth request
+    See also: https://www.rabbitmq.com/management.html
     """
 
-    username = request.form.get('username')
+    username = request.form.get('username', None)
     if username is None:
-        return Response("deny")
+        return Response('deny')
 
     if intention == 'user':
         password = request.form.get('password')
         if username == system_username and password == system_password:
-            return Response("allow")
+            return Response('allow')
         else:
             user = Users().find_one({'username': username}, {'_id': 0, 'password_hash': 1, 'scope.rabbitmq': 1})
+            password_hash = user.get('password_hash', '')
             tags = user.get('scope', {}).get('rabbitmq', [])
-            if user is not None and check_password_hash(user['password_hash'], password):
+            if user is not None and check_password_hash(password_hash, password):
                 tags = ['allow'] + tags
                 return Response(' '.join(tags))
             else:
                 return Response("deny")
     elif intention == 'vhost' or intention == 'resource' or intention == 'topic':
-        vhost = request.form.get('vhost')
-        if username == system_username and vhost == 'zimfarm':
+        vhost = request.form.get('vhost', None)
+        if vhost != 'zimfarm':
+            return Response("deny")
+
+        if username == system_username:
             return Response("allow")
         else:
-            user = Users().find_one({'username': username}, {'_id': 0})
-            if user is not None and vhost == 'zimfarm':
-                return Response("allow")
-            else:
-                return Response("deny")
+            user = Users().find_one({'username': username}, {'_id': 1})
+            return Response("allow" if user is not None else "deny")
     else:
         return Response("deny")
