4abccd431b41128965c28d55d7ac0b2e3c551875
Chris Li <chrisshwli@gmail.com>
2018-06-28 14:53:42 -0400

    more frequent beat update

--- dispatcher/beat/app/main.py
+++ dispatcher/beat/app/main.py
@@ -83,5 +83,5 @@ if __name__ == '__main__':
     url = 'amqp://{username}:{password}@rabbit:5672/zimfarm'.format(username=system_username, password=system_password)
     app = Celery(main='zimfarm', broker=url)
     app.conf.beat_scheduler = MongoScheduler
-    app.conf.beat_max_loop_interval = timedelta(minutes=10).seconds
+    app.conf.beat_max_loop_interval = timedelta(minutes=2).seconds
     app.start(argv=['celery', 'beat', '-l', 'debug'])
