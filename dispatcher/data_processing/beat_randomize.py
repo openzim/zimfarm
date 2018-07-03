55c3ed82057595c19692fd0125ab14092357999b
Chris Li <chrisshwli@gmail.com>
2018-07-03 11:49:24 -0400

    data processing

---
+++ dispatcher/data_processing/beat_randomize.py
@@ -0,0 +1,28 @@
+import requests
+from random import randint
+
+
+def randomize(token: str):
+    response = requests.get('https://farm.openzim.org/api/schedules/',
+                            params={'limit': 60}, headers={'token': token})
+
+    assert response.status_code == 200
+    response_json = response.json()
+    schedule_names = [schedule['name'] for schedule in response_json['items']]
+
+    for name in schedule_names:
+        config = {
+            'minute': randint(0, 3) * 15,
+            'hour': randint(0, 23),
+            'day_of_week': randint(0, 6)
+        }
+        request_json = {
+            'type': 'crontab',
+            'config': config
+        }
+        url = 'https://farm.openzim.org/api/schedules/{}/beat'.format(name)
+        response = requests.patch(url, headers={'token': token}, json=request_json)
+        print('{}, status_code={}'.format(name, response.status_code))
+
+
+randomize(token='')
--- dispatcher/data_processing/mongo_remote.py
+++ dispatcher/data_processing/mongo_remote.py
@@ -2,21 +2,21 @@ from sshtunnel import SSHTunnelForwarder
 from pymongo import MongoClient
 from pycountry import languages
 
-# with SSHTunnelForwarder('farm.openzim.org',
-#                         ssh_username='chris',
-#                         ssh_pkey='~/.ssh/id_rsa',
-#                         remote_bind_address=('127.0.0.1', 27017)) as tunnel:
-#
-#     client = MongoClient('127.0.0.1', tunnel.local_bind_port)
-#     collection = client['Zimfarm']['schedules']
-#
-#     documents = [document for document in collection.find({})]
-#     for document in documents:
-#         language = languages.lookup(document['language'])
-#
-#         collection.update_one({'_id': document['_id']}, {'$set': {
-#             'language': language.alpha_3
-#         }})
+with SSHTunnelForwarder('farm.openzim.org',
+                        ssh_username='chris',
+                        ssh_pkey='~/.ssh/id_rsa',
+                        remote_bind_address=('127.0.0.1', 27017)) as tunnel:
+
+    client = MongoClient('127.0.0.1', tunnel.local_bind_port)
+    collection = client['Zimfarm']['schedules']
+
+    documents = [document for document in collection.find({})]
+    for document in documents:
+        language = languages.lookup(document['language'])
+
+        collection.update_one({'_id': document['_id']}, {'$set': {
+            'language': language.alpha_3
+        }})
 
 
 print({language.alpha_3: language.name for language in list(languages)})
