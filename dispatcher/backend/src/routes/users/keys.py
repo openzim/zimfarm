8dc370aa9c98d1ecdc94f899c3b808979f4d9b1d
Chris Li <chrisshwli@gmail.com>
2018-09-24 00:58:01 -0400

    add todo

--- dispatcher/backend/src/routes/users/keys.py
+++ dispatcher/backend/src/routes/users/keys.py
@@ -8,6 +8,7 @@ from utils.mongo import Users
 @authenticate
 @bson_object_id(['user_id'])
 def list(user_id: ObjectId, user: dict):
+    # TODO: check permission
     ssh_keys = Users().find_one({'_id': user_id}, {'ssh_keys': 1}).get('ssh_keys', [])
     return jsonify(ssh_keys)
 
