a143c073e65cbc85b75898b5b4ea51a2de37e06a
chris <chris@kiwix.org>
2018-11-11 23:55:04 -0500

    add sftp client

--- worker/app/worker/utils/sftp.py
+++ worker/app/worker/utils/sftp.py
@@ -3,7 +3,7 @@ import os
 import paramiko
 
 
-class Client:
+class SFTPClient:
     def __init__(self, hostname: str, port: int, username: str, private_key: str):
         """Create an instance of SFTP client.
 
