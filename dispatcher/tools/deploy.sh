2009c3646aa49f4fe0636afbe3cddf4649b7fe24
chris <chris@kiwix.org>
2018-11-26 22:53:12 -0500

    deployment

--- dispatcher/tools/deploy.sh
+++ dispatcher/tools/deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-if [ ! -d "/data/nginx" ]; then
-  sudo mkdir -p /data/nginx
-  sudo curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /data/nginx/nginx.tmpl
-fi
+git pull
+docker-compose -f ./../docker-compose2.yml -d up
