70c98022c250f0465eafb297bc51335d8161b632
chris <chris@kiwix.org>
2018-11-27 19:49:22 -0500

    bash generate system password

--- dispatcher/deploy.sh
+++ dispatcher/deploy.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
 git pull
+
+# create .env file if not exist
+if [[ ! -f .env ]]
+then
+    touch .env
+fi
+
+if ! grep -q SYSTEM_PASSWORD .env
+then
+    sed -i -e 'SYSTEM_PASSWORD=${pwgen -Bs1 12}' .env
+fi
+
 docker-compose -f docker-compose2.yml up --build -d
