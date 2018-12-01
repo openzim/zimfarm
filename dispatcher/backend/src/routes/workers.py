37aeed8a0e4d3686bcaebce450896bb7b352a2d8
chris <chris@kiwix.org>
2018-12-01 12:52:30 -0500

    better conforming to OAUTH2

--- dispatcher/backend/src/routes/workers.py
+++ dispatcher/backend/src/routes/workers.py
@@ -5,14 +5,14 @@ from celery.app.control import Inspect
 
 from app import celery
 from utils.mongo import Schedules
-from . import authenticate, bson_object_id, errors
+from . import authenticate2, bson_object_id, errors
 
 
 blueprint = Blueprint('workers', __name__, url_prefix='/api/workers')
 
 
 @blueprint.route("/", methods=["GET"])
-@authenticate
+@authenticate2
 def list_workers(user: dict):
     inspect: Inspect = celery.control.inspect()
     return jsonify(inspect.ping())
