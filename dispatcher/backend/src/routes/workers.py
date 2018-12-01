0abaf936b967b3707a3d14cef81bfb08e2834db7
chris <chris@kiwix.org>
2018-12-01 17:06:23 -0500

    list workers

--- dispatcher/backend/src/routes/workers.py
+++ dispatcher/backend/src/routes/workers.py
@@ -4,7 +4,7 @@ from jsonschema import validate, ValidationError
 from celery.app.control import Inspect
 
 from app import celery
-from utils.mongo import Schedules
+from utils.mongo import Workers
 from . import authenticate2, bson_object_id, errors
 
 from utils.token import AccessToken
@@ -16,5 +16,6 @@ blueprint = Blueprint('workers', __name__, url_prefix='/api/workers')
 @blueprint.route("/", methods=["GET"])
 @authenticate2
 def list_workers(token: AccessToken.Payload):
-    inspect: Inspect = celery.control.inspect()
-    return jsonify(inspect.ping())
+    workers = Workers()
+    workers = [workers for workers in workers.find({}, {'_id': 1, 'hostname': 1, 'status': 1, 'session': 1})]
+    return jsonify(workers)
