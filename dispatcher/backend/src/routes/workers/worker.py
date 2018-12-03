771b70655047f10a3acd2aa7e5b856d27f10c5f5
chris <chris@kiwix.org>
2018-12-02 22:46:43 -0500

    update worker routes

--- dispatcher/backend/src/routes/workers/worker.py
+++ dispatcher/backend/src/routes/workers/worker.py
@@ -5,6 +5,16 @@ from utils.token import AccessToken
 
 
 def list(token: AccessToken.Payload):
+    projection = {
+        '_id': 1,
+        'hostname': 1,
+        'status': 1,
+        'session': 1,
+        # 'heartbeats': {'$slice': -1},
+        # 'load_average.1_min': {'$slice': -1},
+        # 'load_average.5_mins': {'$slice': -1},
+        # 'load_average.15_mins': {'$slice': -1},
+    }
     workers = Workers()
-    workers = [workers for workers in workers.find({}, {'_id': 1, 'hostname': 1, 'status': 1, 'session': 1})]
+    workers = [workers for workers in workers.find({}, projection)]
     return jsonify(workers)
