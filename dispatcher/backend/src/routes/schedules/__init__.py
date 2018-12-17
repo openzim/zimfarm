785ad35b083f273ee5239fdec03b316d45bf98d6
chris <chris@kiwix.org>
2018-12-16 23:50:50 -0500

    WIP schedule routes

--- dispatcher/backend/src/routes/schedules/__init__.py
+++ dispatcher/backend/src/routes/schedules/__init__.py
@@ -1,6 +1,10 @@
 import flask
 
+from . import schedule
+
 
 class Blueprint(flask.Blueprint):
     def __init__(self):
         super().__init__('schedules', __name__, url_prefix='/api/schedules')
+
+        self.add_url_rule('/', 'list_schedules', schedule.list, methods=['GET'])
