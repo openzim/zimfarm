c13d0c8d5ad39e63ad7a4960eebc6eca49956c87
chris <chris@kiwix.org>
2018-12-26 08:39:29 -0500

    WIP schedule beat routes - follow up

--- dispatcher/backend/src/routes/schedules/beat.py
+++ dispatcher/backend/src/routes/schedules/beat.py
@@ -1,4 +1,5 @@
 import trafaret
+from celery.schedules import crontab, ParseException
 from flask import jsonify, request, Response
 
 from errors.http import ScheduleNotFound, InvalidRequestJSON
@@ -39,25 +40,33 @@ class BeatRoute(BaseRoute, URLComponent):
         - month_of_year
         """
 
-        try:
-            query = self.get_schedule_query(schedule)
-            request_json = request.get_json()
-            if request_json is None:
-                raise InvalidRequestJSON()
+        query = self.get_schedule_query(schedule)
+        request_json = request.get_json()
+        if request_json is None:
+            raise InvalidRequestJSON()
 
-            beat_type = request_json.get('type')
-            config = request_json.get('config')
+        beat_type = request_json.get('type')
+        config = request_json.get('config')
 
-            if beat_type == 'crontab':
+        if beat_type == 'crontab':
+            try:
                 config = CrontabValidator().check(config)
-                beat = {'type': beat_type, 'config': config}
-                matched_count = Schedules().update_one(query, {'$set': {'beat': beat}}).matched_count
-            else:
-                raise InvalidRequestJSON('Invalid beat type')
+                crontab(minute=config.get('minute', '*'),
+                        hour=config.get('hour', '*'),
+                        day_of_week=config.get('day_of_week', '*'),
+                        day_of_month=config.get('day_of_month', '*'),
+                        month_of_year=config.get('month_of_year', '*'))
+            except trafaret.DataError as e:
+                raise InvalidRequestJSON(str(e.error))
+            except ParseException as e:
+                raise InvalidRequestJSON(str(e))
 
-            if matched_count:
-                return Response()
-            else:
-                raise ScheduleNotFound()
-        except trafaret.DataError as e:
-            raise InvalidRequestJSON(str(e.error))
+            beat = {'type': beat_type, 'config': config}
+            matched_count = Schedules().update_one(query, {'$set': {'beat': beat}}).matched_count
+        else:
+            raise InvalidRequestJSON('Invalid beat type')
+
+        if matched_count:
+            return Response()
+        else:
+            raise ScheduleNotFound()
