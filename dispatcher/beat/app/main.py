f1b3f6e1b4970e74df072a5d12b5d36dc87c262a
Chris Li <chrisencoded@gmail.com>
2019-01-02 16:50:05 -0500

    Backend: task queue (#82)
    * refactor
    
    * add tasks class
    
    * restructuring
    
    * restructuring
    
    * restructuring
    
    * check mongo boot
    
    * write task history to task collection
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * ObjectId as task_id
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing
    
    * WIP: task routing

--- dispatcher/beat/app/main.py
+++ dispatcher/beat/app/main.py
@@ -1,89 +1,57 @@
 import os
-from datetime import datetime, timedelta
-from typing import Optional
+import sys
+from datetime import timedelta
+from time import sleep
 
+import amqp
 from celery import Celery
-from celery.beat import Scheduler, ScheduleEntry, crontab
-from celery.schedules import BaseSchedule
-from celery.utils.log import get_logger
+from kombu import Queue, Exchange
+from pymongo.errors import ServerSelectionTimeoutError
 
-from mongo import Schedules
-
-
-class MongoSchedulerEntry(ScheduleEntry):
-    def __init__(self, name: str, task_name: str, last_run_at: datetime,
-                 total_run_count: int, schedule: BaseSchedule, app: Celery, *args, **kwargs):
-        super().__init__(name=name, task=task_name, last_run_at=last_run_at, total_run_count=total_run_count,
-                         schedule=schedule, args=args, kwargs=kwargs, options={}, app=app)
-        self.logger = get_logger('zimfarm')
-
-    @classmethod
-    def from_document(cls, app: Celery, document: dict):
-        schedule_name = document.get('name', None)
-        task_name = document.get('task', {}).get('name', None)
-
-        offliner_config = document.get('offliner', {}).get('config', None)
-        last_run = document.get('last_run', None)
-        total_run = document.get('total_run', 0)
-
-        if schedule_name is None or task_name is None or offliner_config is None:
-            return None
-
-        schedule = cls.get_schedule(document['beat'])
-        if schedule is None:
-            return None
-
-        return cls(schedule_name, task_name, last_run, total_run, schedule, app, offliner_config=offliner_config)
-
-    @staticmethod
-    def get_schedule(beat: dict) -> Optional[BaseSchedule]:
-        type = beat['type']
-        config = beat['config']
-        if type == 'crontab':
-            return crontab(minute=config.get('minute', '*'),
-                           hour=config.get('hour', '*'),
-                           day_of_week=config.get('day_of_week', '*'),
-                           day_of_month=config.get('day_of_month', '*'),
-                           month_of_year=config.get('month_of_year', '*'))
-        else:
-            return None
-
-    def __next__(self):
-        Schedules().update_one({'name': self.name}, {'$set': {
-                                   'last_run': self.app.now(),
-                                   'total_run': self.total_run_count + 1
-                               }})
-        document = Schedules().find_one({'name': self.name})
-        return self.__class__.from_document(self.app, document)
+from mongo import Client
+from schedules import Scheduler
 
 
-class MongoScheduler(Scheduler):
-    Entry = ScheduleEntry
-    last_update = None
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.logger = get_logger('zimfarm')
-
-    @property
-    def schedule(self) -> dict:
-        if self.last_update is not None and datetime.now() - self.last_update < timedelta(minutes=1):
-            # will use cached data for very frequent access
-            return self.data
-
-        self.data = {document['name']: MongoSchedulerEntry.from_document(app=self.app, document=document)
-                     for document in Schedules().find({'enabled': True})}
-        self.last_update = datetime.now()
-
-        self.logger.debug('Schedules synced, count={}'.format(len(self.data)))
-        return self.data
+def check_mongo_booted() -> bool:
+    retries = 3
+    while retries > 0:
+        try:
+            client = Client()
+            client.server_info()
+            return True
+        except ServerSelectionTimeoutError:
+            retries -= 1
+            sleep(10)
+    return False
 
 
 if __name__ == '__main__':
+    if not check_mongo_booted():
+        sys.exit(1)
+
     system_username = 'system'
     system_password = os.getenv('SYSTEM_PASSWORD', '')
     url = 'amqp://{username}:{password}@rabbit:5672/zimfarm'.format(username=system_username, password=system_password)
+
     app = Celery(main='zimfarm', broker=url)
-    app.conf.beat_scheduler = MongoScheduler
+
+    # configure beat
+    app.conf.beat_scheduler = Scheduler
     app.conf.beat_max_loop_interval = timedelta(minutes=2).seconds
-    app.start(argv=['celery', 'beat', '-l', 'debug'])
+
+    # configure queue
+    offliner_exchange = Exchange('offliner', 'topic')
+    app.conf.task_queues = [
+        Queue('offliner_default', offliner_exchange, routing_key='#'),
+        Queue('offliner_small', offliner_exchange, routing_key='small'),
+        Queue('offliner_medium', offliner_exchange, routing_key='medium'),
+        Queue('offliner_large', offliner_exchange, routing_key='large')
+    ]
+
+    retries = 3
+    while retries > 0:
+        try:
+            app.start(argv=['celery', 'beat', '--loglevel', 'debug'])
+        except amqp.exceptions.AccessRefused:
+            retries -= 1
+            sleep(2)
