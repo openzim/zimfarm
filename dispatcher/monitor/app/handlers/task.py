415ea6c0b28eeb713acd7c790e19f4507b61d20f
chris <chris@kiwix.org>
2019-01-12 11:17:57 -0500

    WIP: logging

--- dispatcher/monitor/app/handlers/task.py
+++ dispatcher/monitor/app/handlers/task.py
@@ -5,13 +5,13 @@ from celery.events.state import Task
 from . import BaseHandler
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class BaseTaskEventHandler(BaseHandler):
     def __call__(self, event):
         self.state.event(event)
-        logger.debug(event)
-        print(event)
+        # logger.debug(event)
 
         task: Task = self.state.tasks.get(event['uuid'])
-        logger.debug(task)
+        logger.debug(task.info())
