0ed79603a423a8432975abf1a22fc5dbf1ab2fbf
Chris Li <chrisencoded@gmail.com>
2019-01-18 00:01:40 -0500

    Frontend schedue detail (#107)
    * add beat and offliner config
    
    * update static
    
    * update frontend

--- dispatcher/frontend/src/app/shared/shared.module.ts
+++ dispatcher/frontend/src/app/shared/shared.module.ts
@@ -1,15 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import cronstrue from 'cronstrue';
 import { TimeAgoPipe } from 'time-ago-pipe';
+
+import { Beat } from '../services/schedules.service';
+
+@Pipe({name: 'beatDescription'})
+export class BeatDescription implements PipeTransform {
+    transform(beat: Beat) {
+        if (beat.type == 'crontab') {
+            let config = beat.config;
+            let cron = Array(config['minute'], config['hour'], config['day_of_month'], config['month_of_year'], config['day_of_week']).join(' ');
+            return cronstrue.toString(cron);
+        } else {
+            return 'Error: unsupported beat type';
+        }
+    }
+}
 
 @NgModule({
     imports: [CommonModule],
     declarations: [
-        TimeAgoPipe
+        TimeAgoPipe,
+        BeatDescription
     ],
     exports: [
-        TimeAgoPipe
+        TimeAgoPipe,
+        BeatDescription
     ]
 })
 export class SharedModule { }
+
