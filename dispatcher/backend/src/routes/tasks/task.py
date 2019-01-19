380529885416117485bd91857859e05dc91e3f2a
chris <chris@kiwix.org>
2019-01-18 19:57:43 -0500

    update implementation

--- dispatcher/backend/src/routes/tasks/task.py
+++ dispatcher/backend/src/routes/tasks/task.py
@@ -22,19 +22,25 @@ class TasksRoute(BaseRoute):
         request_json = request.get_json()
         schedule_name = request_json.get('schedule_name')
 
-        schedule = Schedules().find_one({'name': schedule_name})
+        schedule = Schedules().find_one({'name': schedule_name}, {'config': 1})
+        id = schedule.get('_id')
+        config = schedule.get('config')
 
-        if schedule:
+        if id and config:
             task_id = Tasks().insert_one({
-                'schedule_id': schedule['_id'],
-                'debug': True,
-                'timestamp': {
-                    'created': datetime.utcnow().replace(tzinfo=pytz.utc)
-                }
+                'schedule_id': id,
+                'timestamp': {'created': datetime.utcnow().replace(tzinfo=pytz.utc)}
             }).inserted_id
-            task_name = schedule.get('celery', {}).get('task_name')
-            queue = schedule.get('celery', {}).get('queue')
-            task_kwargs = schedule.get('task', {})
+
+            task_name = config.get('task_name')
+            queue = config.get('queue')
+            offliner = config.get('offliner')
+            warehouse_path = config.get('warehouse_path')
+
+            task_kwargs = {
+                'offliner': offliner,
+                'warehouse_path': warehouse_path
+            }
 
             celery = Celery()
             celery.send_task(name=task_name, args=(), kwargs=task_kwargs, task_id=str(task_id), queue=queue)
