833949cecd6db6e012ddc9b2305f2a32deff8763
chris <chris@kiwix.org>
2019-01-20 12:45:16 -0500

    return file stats

--- worker/app/worker/tasks/mwoffliner.py
+++ worker/app/worker/tasks/mwoffliner.py
@@ -1,5 +1,6 @@
 import docker
 import docker.errors
+from pathlib import Path
 
 from .base import Base
 from ..operations import RunRedis, RunMWOffliner, Upload
@@ -35,16 +36,20 @@ class MWOffliner(Base):
 
             run_mwoffliner = RunMWOffliner(
                 docker_client=docker.from_env(), tag=offliner_image_tag, flags=offliner_flags,
-                task_id=self.short_task_id, working_dir_host=Settings.working_dir_host,
+                task_id=self.task_id, working_dir_host=Settings.working_dir_host,
                 redis_container_name=Settings.redis_name)
             self.logger.info('{name}[{id}] -- Running MWOffliner, mwUrl: {mwUrl}'.format(
-                name=self.name, id=self.short_task_id, mwUrl=offliner_flags['mwUrl']))
+                name=self.name, id=self.task_id, mwUrl=offliner_flags['mwUrl']))
             run_mwoffliner.execute()
 
+            stats = self.get_file_stats()
+
             upload = Upload(warehouse_path, working_dir=Settings.working_dir_container,
-                            short_task_id=self.short_task_id)
-            self.logger.info('{name}[{id}] -- Upload files'.format(name=self.name, id=self.short_task_id))
+                            short_task_id=self.task_id)
+            self.logger.info('{name}[{id}] -- Upload files'.format(name=self.name, id=self.task_id))
             upload.execute()
+
+            return stats
         except docker.errors.APIError:
             pass
         except docker.errors.ImageNotFound:
@@ -52,3 +57,12 @@ class MWOffliner(Base):
         except docker.errors.ContainerError as error:
             # error.stderr
             pass
+
+    def get_file_stats(self):
+        stats = []
+        working_dir = Path(Settings.working_dir_container).joinpath(self.task_id)
+        for file in working_dir.iterdir():
+            if file.is_dir():
+                continue
+            stats.append({'name': file.name, 'size': file.stat().st_size})
+        return stats
