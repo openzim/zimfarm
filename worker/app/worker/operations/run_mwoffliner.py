c20dd6fb84b73fadacab7d985231dcb87dd30fce
chris <chris@kiwix.org>
2019-01-20 11:43:52 -0500

    add support for array flag values

--- worker/app/worker/operations/run_mwoffliner.py
+++ worker/app/worker/operations/run_mwoffliner.py
@@ -11,14 +11,14 @@ class RunMWOffliner(Operation):
 
     name = 'Run Offliner'
 
-    def __init__(self, docker_client: DockerClient, tag: str, config: {}, task_id: str, working_dir_host: str,
+    def __init__(self, docker_client: DockerClient, tag: str, flags: {}, task_id: str, working_dir_host: str,
                  redis_container_name: str):
         tag = 'latest' if not tag else tag
         super().__init__()
         self.docker = docker_client
-        self.config = config
+        self.command = self._get_command(flags)
         self.task_id = task_id
-        self.working_dir_host = Path(working_dir_host)
+        self.working_dir_host = Path(working_dir_host).joinpath(self.task_id).absolute()
         self.redis_container_name = redis_container_name
         self.image_name = 'openzim/mwoffliner:{}'.format(tag)
 
@@ -34,19 +34,21 @@ class RunMWOffliner(Operation):
         self.docker.images.pull(self.image_name)
 
         # run mwoffliner
-        volumes = {self.working_dir_host.joinpath(self.task_id).absolute(): {'bind': '/output', 'mode': 'rw'}}
-        self.std_out = self.docker.containers.run(image=self.image_name,
-                                                  command=self._get_command(self.config),
+        volumes = {self.working_dir_host: {'bind': '/output', 'mode': 'rw'}}
+        self.std_out = self.docker.containers.run(image=self.image_name, command=self.command,
                                                   remove=True, volumes=volumes,
                                                   links={self.redis_container_name: 'redis'},
                                                   name='mwoffliner_{}'.format(self.task_id))
 
+        # get operation output
+
+
     @staticmethod
-    def _get_command(config: {}):
-        config['redis'] = 'redis://redis'
-        config['outputDirectory'] = '/output'
+    def _get_command(flags: {}):
+        flags['redis'] = 'redis://redis'
+        flags['outputDirectory'] = '/output'
         params: [str] = []
-        for key, value in config.items():
+        for key, value in flags.items():
             if value is True:
                 params.append('--{name}'.format(name=key))
             elif isinstance(value, list):
--- worker/app/worker/tasks/mwoffliner.py
+++ worker/app/worker/tasks/mwoffliner.py
@@ -29,27 +29,26 @@ class MWOffliner(Base):
         offliner_image_tag = offliner.get('image_tag', 'latest')
         offliner_flags = offliner.get('flags', {})
 
-        operations = [
-            RunRedis(docker_client=docker.from_env(), container_name=Settings.redis_name),
-            RunMWOffliner(docker_client=docker.from_env(), tag=offliner_image_tag, config=offliner_flags,
-                          task_id=self.short_task_id, working_dir_host=Settings.working_dir_host,
-                          redis_container_name=Settings.redis_name),
-            Upload(warehouse_path, working_dir=Settings.working_dir_container, short_task_id=self.short_task_id)
-        ]
+        try:
+            run_redis = RunRedis(docker_client=docker.from_env(), container_name=Settings.redis_name)
+            run_redis.execute()
 
-        for index, operation in enumerate(operations):
-            if isinstance(operation, RunMWOffliner):
-                self.logger.info('{name}[{id}] -- Running MWOffliner, mwUrl: {mwUrl}'.format(
-                    name=self.name, id=self.short_task_id, mwUrl=offliner_flags['mwUrl']))
-            if isinstance(operation, Upload):
-                self.logger.info('{name}[{id}] -- Upload files'.format(name=self.name, id=self.short_task_id))
+            run_mwoffliner = RunMWOffliner(
+                docker_client=docker.from_env(), tag=offliner_image_tag, flags=offliner_flags,
+                task_id=self.short_task_id, working_dir_host=Settings.working_dir_host,
+                redis_container_name=Settings.redis_name)
+            self.logger.info('{name}[{id}] -- Running MWOffliner, mwUrl: {mwUrl}'.format(
+                name=self.name, id=self.short_task_id, mwUrl=offliner_flags['mwUrl']))
+            run_mwoffliner.execute()
 
-            try:
-                operation.execute()
-            except docker.errors.APIError:
-                pass
-            except docker.errors.ImageNotFound:
-                pass
-            except docker.errors.ContainerError as error:
-                # error.stderr
-                pass
+            upload = Upload(warehouse_path, working_dir=Settings.working_dir_container,
+                            short_task_id=self.short_task_id)
+            self.logger.info('{name}[{id}] -- Upload files'.format(name=self.name, id=self.short_task_id))
+            upload.execute()
+        except docker.errors.APIError:
+            pass
+        except docker.errors.ImageNotFound:
+            pass
+        except docker.errors.ContainerError as error:
+            # error.stderr
+            pass
