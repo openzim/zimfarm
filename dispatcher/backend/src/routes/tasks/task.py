28e0b2a2fb0f6cdf23ef868c79d595b92ca00f16
Chris Li <chris@kiwix.org>
2019-01-27 17:22:57 -0500

    updates task route

--- dispatcher/backend/src/routes/tasks/task.py
+++ dispatcher/backend/src/routes/tasks/task.py
@@ -1,13 +1,9 @@
-from datetime import datetime
-import pytz
-
 from flask import request, Response
 
+from errors.http import InvalidRequestJSON
+from models import ScheduleService
 from routes import authenticate
 from routes.base import BaseRoute
-from errors.http import InvalidRequestJSON
-from utils.celery import Celery
-from mongo import Tasks, Schedules
 
 
 class TasksRoute(BaseRoute):
@@ -23,31 +19,8 @@ class TasksRoute(BaseRoute):
         if not request_json:
             raise InvalidRequestJSON()
 
-        schedule_name = request_json.get('schedule_name')
+        schedule_names = request_json.get('schedule_names', [])
+        for schedule_name in schedule_names:
+            ScheduleService.send_task(schedule_name)
 
-        schedule = Schedules().find_one({'name': schedule_name}, {'config': 1})
-        id = schedule.get('_id')
-        config = schedule.get('config')
-
-        if id and config:
-            task_id = Tasks().insert_one({
-                'schedule_id': id,
-                'timestamp': {'created': datetime.utcnow().replace(tzinfo=pytz.utc)}
-            }).inserted_id
-
-            task_name = config.get('task_name')
-            queue = config.get('queue')
-            offliner = config.get('offliner')
-            warehouse_path = config.get('warehouse_path')
-
-            task_kwargs = {
-                'offliner': offliner,
-                'warehouse_path': warehouse_path
-            }
-
-            celery = Celery()
-            celery.send_task(name=task_name, args=(), kwargs=task_kwargs, task_id=str(task_id), queue=queue)
-
-            return Response()
-        else:
-            raise InvalidRequestJSON()
+        return Response()
