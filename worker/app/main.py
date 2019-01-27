0c132bff152eb05b53ca9b5e3c496f5a27e78651
Chris Li <chris@kiwix.org>
2019-01-27 08:21:53 -0500

    logging

--- worker/app/main.py
+++ worker/app/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # setting up logging
     log_path = os.getenv('LOG_PATH', '/worker.log')
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s',
+                        format='[%(asctime)s: %(levelname)s] %(message)s',
                         handlers=[logging.StreamHandler(),
                                   logging.FileHandler(log_path, mode='w')])
     logging.getLogger("paramiko").setLevel(logging.WARNING)
