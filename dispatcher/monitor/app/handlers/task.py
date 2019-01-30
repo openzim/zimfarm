769690de4850f31e354d920963cdd87b53a90342
Chris Li <chris@kiwix.org>
2019-01-29 23:19:07 -0500

    update task success handler

--- dispatcher/monitor/app/handlers/task.py
+++ dispatcher/monitor/app/handlers/task.py
@@ -1,16 +1,15 @@
-import logging
 import ast
+import logging
+from typing import Optional
 
 from bson.objectid import ObjectId, InvalidId
 from celery.events.state import Task
 
+from entities import TaskStatus
 from mongo import Tasks
 from . import BaseHandler
-from entities import TaskStatus
-from typing import Optional
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
 class BaseTaskEventHandler(BaseHandler):
@@ -35,8 +34,12 @@ class TaskSucceededEventHandler(BaseTaskEventHandler):
         task_id = self.get_task_id(task)
         result = ast.literal_eval(task.result)
 
-        logger.info('Task Succeeded: {}'.format(task_id))
+        logger.info('Task Succeeded: {}, {}, {}'.format(task_id, task.runtime, task.timestamp))
+
+        update = {
+            'status': TaskStatus.succeeded,
+            'files': result
+        }
 
         if task_id:
-            Tasks().update_one({'_id': task_id},
-                               {'$set': {'status': TaskStatus.succeeded, 'files': result}})
+            Tasks().update_one({'_id': task_id}, {'$set': update})
