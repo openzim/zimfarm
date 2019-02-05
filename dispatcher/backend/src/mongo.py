6a27d0300f77ad1d20f0b0ef6097fe58616ec976
Chris Li <chris@kiwix.org>
2019-02-04 19:24:01 -0500

    add index & validator to users collection

--- dispatcher/backend/src/mongo.py
+++ dispatcher/backend/src/mongo.py
@@ -7,7 +7,7 @@ from pymongo.collection import Collection as BaseCollection
 
 class Client(MongoClient):
     def __init__(self):
-        super().__init__(host=os.getenv('MONGO_HOSTNAME', 'mongo'))
+        super().__init__(host=os.getenv('MONGO_HOSTNAME', 'localhost'))
 
 
 class Database(BaseDatabase):
@@ -18,6 +18,39 @@ class Database(BaseDatabase):
 class Users(BaseCollection):
     def __init__(self):
         super().__init__(Database(), 'users')
+
+    def initialize(self):
+        self.create_index('username', name='username', unique=True)
+        self.create_index('email', name='email', unique=True)
+        self.create_index('ssh_keys.fingerprint', name='ssh_keys.fingerprint',
+                          partialFilterExpression={'ssh_keys': {'$exists': True}})
+
+        schema = {
+            "bsonType": "object",
+            "required": ["username", "password_hash"],
+            "properties": {
+                "username": {"bsonType": "string"},
+                "password_hash": {"bsonType": "string"},
+                "email": {"bsonType": "string"},
+                "ssh_keys": {
+                    "bsonType": "array",
+                    "items": {
+                        "bsonType": "object",
+                        "required": ["name", "fingerprint", "key", "type", "added", "last_used"],
+                        "properties": {
+                            "name": {"bsonType": "string"},
+                            "fingerprint": {"bsonType": "string"},
+                            "key": {"bsonType": "string"},
+                            "type": {"enum": ["RSA"]},
+                            "added": {"bsonType": "date"},
+                            "last_used": {"bsonType": "date"},
+                        },
+                        "additionalProperties": False
+                    }
+                }
+            }
+        }
+        self.database.command({'collMod': 'users', 'validator': {'$jsonSchema': schema}})
 
 
 class RefreshTokens(BaseCollection):
