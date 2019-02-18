ecba338931ef0a60b543134a336e49968307e959
Chris Li <chris@kiwix.org>
2019-02-17 23:23:45 -0500

    remove old code

--- dispatcher/frontend/src/app/app.module.ts
+++ dispatcher/frontend/src/app/app.module.ts
@@ -8,9 +8,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RootComponent, AppComponent } from './components/components';
 import { LoginComponent } from './components/login/login.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
-import { QueueComponent } from './components/queue/queue.component';
-import { WorkerComponent } from './components/worker/worker.component';
-import { LogComponent } from './components/log/log.component';
 import { UserComponent } from './components/user/user.component';
 
 import { AuthGuard } from './guards/auth.guard';
@@ -23,12 +20,9 @@ const routes: Routes = [
         component: AppComponent,
         canActivate: [AuthGuard],
         children: [
-            { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' },
-            { path: 'queue', component: QueueComponent },
-            { path: 'worker', component: WorkerComponent },
-            { path: 'log', component: LogComponent },
+            { path: 'schedules', loadChildren: './schedule/schedule.module#ScheduleModule' },
             { path: 'user', component: UserComponent },
-            { path: '**', redirectTo: 'schedule' }
+            { path: '**', redirectTo: 'schedules' }
         ]
     },
     
@@ -47,9 +41,6 @@ const routes: Routes = [
         AppComponent,
         LoginComponent,
         NavigationBarComponent,
-        QueueComponent,
-        WorkerComponent,
-        LogComponent,
         UserComponent
     ],
     providers: [
