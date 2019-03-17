c21575a150bd10d90e8a5179df0de91901327b94
Chris Li <chris@kiwix.org>
2019-03-16 21:00:18 -0400

    db migration

--- dispatcher/backend/src/tools/database.py
+++ dispatcher/backend/src/tools/database.py
@@ -42,12 +42,32 @@ def update_most_recent_task(client: MongoClient):
             client['Zimfarm']['schedules'].update_one(schedule_filter, update)
 
 
+def fix(client: MongoClient):
+    tasks = client['Zimfarm']['tasks']
+    schedules = client['Zimfarm']['schedules']
+    for task in tasks.find({}):
+        if 'schedule_id' in task:
+            schedule_id = task['schedule_id']
+            schedule = schedules.find_one({'_id': schedule_id})
+        elif 'schedule_name' in task:
+            schedule_name = task['schedule_name']
+            schedule = schedules.find_one({'name': schedule_name})
+        else:
+            continue
 
+        if schedule is None:
+            print(task)
+            continue
+
+        tasks.update_one({'_id': task['_id']}, {
+            '$set': {'schedule': {'_id': schedule['_id'], 'name': schedule['name']}},
+            '$unset': {'schedule_id': 1, 'schedule_name': 1}
+        })
 
 
 if __name__ == '__main__':
     with SSHTunnelForwarder('farm.openzim.org', ssh_username='chris', ssh_pkey="/Users/chrisli/.ssh/id_rsa",
                             remote_bind_address=('127.0.0.1', 27017), local_bind_address=('0.0.0.0', 27018)) as tunnel:
         with MongoClient(port=27018) as client:
-            get_schedule_to_run_json(client)
+            fix(client)
     print('FINISH!')
