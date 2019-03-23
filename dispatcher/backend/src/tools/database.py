2739352dde6252455cf104eef763acee5fa24851
Chris Li <chris@kiwix.org>
2019-03-23 09:54:43 -0400

    log dump

--- dispatcher/backend/src/tools/database.py
+++ dispatcher/backend/src/tools/database.py
@@ -48,10 +48,36 @@ def update_most_recent_task(client: MongoClient):
             client['Zimfarm']['schedules'].update_one(schedule_filter, update)
 
 
+def get_most_recent_failed_task(client: MongoClient):
+    import re
+
+    tasks = client['Zimfarm']['tasks']
+    schedules = client['Zimfarm']['schedules']
+    schedules_to_tun = []
+    for schedule in schedules.find({'most_recent_task.status': 'failed'}):
+        schedule_name = schedule['name']
+        task = tasks.find_one({'_id': schedule['most_recent_task']['_id']})
+        events = task.get('events', [])
+        traceback = events[-1]['traceback']
+
+        reg = re.compile(r'non-zero exit status (\d*): ')
+        result = reg.search(traceback)
+        if result:
+            code = result.group(1)
+        else:
+            schedules_to_tun.append(schedule_name)
+
+        # with open(f'traceback/{code}_{schedule_name}.log', 'w') as file:
+        #     file.write(traceback.replace('\\n', '\n'))
+
+    print(json.dumps(schedules_to_tun))
+
+
 if __name__ == '__main__':
     with SSHTunnelForwarder('farm.openzim.org', ssh_username='chris', ssh_pkey="/Users/chrisli/.ssh/id_rsa",
                             remote_bind_address=('127.0.0.1', 27017), local_bind_address=('0.0.0.0', 27018)) as tunnel:
         with MongoClient(port=27018) as client:
-            excluded = get_sent_task_schedule_ids(client)
-            get_schedules_to_run(excluded)
+            # excluded = get_sent_task_schedule_ids(client)
+            # get_schedules_to_run(excluded)
+            get_most_recent_failed_task(client)
     print('FINISH!')
