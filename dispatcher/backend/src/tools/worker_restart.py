736f0642fa472a9d1fc04d20940f613c39fc55fd
Chris Li <chrisencoded@gmail.com>
2019-04-15 20:24:46 -0400

    Dispatcher/tools (#175)
    * purge
    
    * restart
    
    * db
    
    * worker restart
    
    * db migration
    
    * remove
    
    * switch back command
    
    * get schedules to run
    
    * get schedules
    
    * log dump
    
    * april
    
    * tool

---
+++ dispatcher/backend/src/tools/worker_restart.py
@@ -0,0 +1,8 @@
+from os import getenv
+
+username = getenv('ZIMFARM_USERNAME')
+username = getenv('ZIMFARM_PASSWORD')
+username = getenv('ZIMFARM_WORKING_DIR')
+username = getenv('ZIMFARM_NODE_NAME')
+username = getenv('ZIMFARM_QUEUES')
+username = getenv('ZIMFARM_CONCURRENCY')
