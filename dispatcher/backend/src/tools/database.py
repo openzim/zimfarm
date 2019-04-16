4d3e08729ea054b4c679f6f5b0ebd8cecc2fa001
Chris Li <chrisencoded@gmail.com>
2019-04-15 20:24:46 -0400

    Dispatcher/tools (#175)
    * purge
    
    * restart
    
    * db
    
    * worker restart
    
    * db migration
    
    * remove
    
    * switch back command
    
    * get schedules to run
    
    * get schedules
    
    * log dump
    
    * april
    
    * tool

--- dispatcher/backend/src/tools/database.py
+++ dispatcher/backend/src/tools/database.py
@@ -5,68 +5,31 @@ import pytz
 import json
 
 
-def get_schedule_to_run_json(client: MongoClient):
-    schedules_collection = client['Zimfarm']['schedules']
-
+def get_succeed_task_schedule_ids(client: MongoClient) -> set:
     tasks = client['Zimfarm']['tasks']
-    tasks_succeed = tasks.find({
-        "timestamp.succeeded": {
-            '$gte': datetime(2019, 3, 1, 00, 00, 00, tzinfo=pytz.utc)
-        }, "files": {"$exists": True}}, {'schedule_id': 1})
-    schedules_id_succeed = [task['schedule_id'] for task in tasks_succeed]
-    schedules_id_succeed = set(schedules_id_succeed)
-
-    schedules = schedules_collection.find({"tags": ["nopic", "novid"]}, {"_id": 1, "name": 1, "language.code": 1})
-    schedules_to_run = [schedule for schedule in schedules
-                        if schedule['_id'] not in schedules_id_succeed]
-    schedules_to_run = [schedule for schedule in schedules_to_run if 'e' <= schedule['language']['code'][0] < 'n']
-    schedules_to_run = [schedule['name'] for schedule in schedules_to_run]
-    print(len(schedules_to_run))
-    encoded = json.dumps(schedules_to_run)
-    print(encoded)
+    tasks_sent = tasks.find({'timestamp.succeeded': {'$gte': datetime(2019, 4, 1, 00, 00, 00, tzinfo=pytz.utc)},
+                             'files': {'$exists': True}},
+                            {'schedule': 1})
+    return set([task['schedule']['_id'] for task in tasks_sent])
 
 
-def assemble_manual_run_request(client: MongoClient):
-    schedules_collection = client['Zimfarm']['schedules']
-    schedules = schedules_collection.find({"category": "wikiversity", "tags": ["nopic", "novid"]},
-                                          {"_id": 1, "name": 1})
+def get_sent_task_schedule_ids(client: MongoClient) -> set:
+    tasks = client['Zimfarm']['tasks']
+    tasks_sent = tasks.find({'timestamp.sent': {'$gte': datetime(2019, 4, 1, 00, 00, 00, tzinfo=pytz.utc)}},
+                            {'schedule': 1})
+    return set([task['schedule']['_id'] for task in tasks_sent])
+
+
+def get_schedules_to_run(schedules_excluded: set):
+    schedules = client['Zimfarm']['schedules']
+    schedules = schedules.find({}, {'name': 1, 'language': 1})
+    schedules = [schedule for schedule in schedules if schedule['_id'] not in schedules_excluded]
+    schedules = [schedule for schedule in schedules if 'b' <= schedule['language']['code'][0] <= 'e']
     schedule_names = [schedule['name'] for schedule in schedules]
+
+    print(len(schedule_names))
     encoded = json.dumps(schedule_names)
     print(encoded)
-
-
-def nopic_to_nopic_and_novid(client: MongoClient):
-    schedules_collection = client['Zimfarm']['schedules']
-    schedules = schedules_collection.find({"category": "wikipedia", "tags": ["nopic"]}, {"_id": 1, "name": 1})
-
-    update_count = 0
-    for schedule in schedules:
-        schedule_id = schedule['_id']
-        schedule_name = schedule['name'].replace('_nopic', '')
-        result = schedules_collection.update_one(
-            {'_id': schedule_id},
-            {'$set': {
-                'name': schedule_name,
-                'config.queue': 'large',
-                'config.flags.useCache': True,
-                'config.flags.format': ["nopic", "novid"],
-                'tags': ["nopic", "novid"]}})
-        update_count += result.modified_count
-    print(f"Update count: {update_count}")
-
-
-def remove_duplicated_novid_schedule(client: MongoClient):
-    schedules_collection = client['Zimfarm']['schedules']
-    schedules = schedules_collection.find({"tags": ["nopic", "novid"]}, {"category": 1, "language": 1})
-
-    delete_count = 0
-    for schedule in schedules:
-        result = schedules_collection.delete_one({
-            'category': schedule['category'],
-            'language.code': schedule['language']['code'],
-            'tags': ['novid']})
-        delete_count += result.deleted_count
-    print(f"Delete count: {delete_count}")
 
 
 def update_most_recent_task(client: MongoClient):
@@ -85,10 +48,35 @@ def update_most_recent_task(client: MongoClient):
             client['Zimfarm']['schedules'].update_one(schedule_filter, update)
 
 
+def get_most_recent_failed_task(client: MongoClient):
+    import re
+
+    tasks = client['Zimfarm']['tasks']
+    schedules = client['Zimfarm']['schedules']
+    schedules_to_tun = []
+    for schedule in schedules.find({'most_recent_task.status': 'failed'}):
+        schedule_name = schedule['name']
+        task = tasks.find_one({'_id': schedule['most_recent_task']['_id']})
+        events = task.get('events', [])
+        traceback = events[-1]['traceback']
+
+        reg = re.compile(r'non-zero exit status (\d*): ')
+        result = reg.search(traceback)
+        if result:
+            code = result.group(1)
+        else:
+            schedules_to_tun.append(schedule_name)
+
+        # with open(f'traceback/{code}_{schedule_name}.log', 'w') as file:
+        #     file.write(traceback.replace('\\n', '\n'))
+
+    print(json.dumps(schedules_to_tun))
+
 
 if __name__ == '__main__':
     with SSHTunnelForwarder('farm.openzim.org', ssh_username='chris', ssh_pkey="/Users/chrisli/.ssh/id_rsa",
                             remote_bind_address=('127.0.0.1', 27017), local_bind_address=('0.0.0.0', 27018)) as tunnel:
         with MongoClient(port=27018) as client:
-            update_most_recent_task(client)
+            excluded = get_succeed_task_schedule_ids(client)
+            get_schedules_to_run({})
     print('FINISH!')
