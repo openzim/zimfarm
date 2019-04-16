0c6cfd4a3e0ec43dad82d549e38ffbf64ff6577f
Chris Li <chris@kiwix.org>
2019-04-15 23:03:03 -0400

    task status entity

--- dispatcher/backend/src/common/entities.py
+++ dispatcher/backend/src/common/entities.py
@@ -4,3 +4,7 @@ class TaskStatus:
     started = 'started'
     succeeded = 'succeeded'
     failed = 'failed'
+
+    @classmethod
+    def all(cls):
+        return [cls.sent, cls.received, cls.started, cls.succeeded, cls.failed]
