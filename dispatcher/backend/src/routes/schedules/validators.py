8a1d1584691e5ffdc0e24abbc6bfd26ba14cd9e2
ChrisLi <chris.li@datarobot.com>
2019-05-06 15:20:47 -0400

    automactic/remove beats route (#199)
    * rename trafaret
    
    * remove beat route
    
    * remove beat route

--- dispatcher/backend/src/routes/schedules/validators.py
+++ dispatcher/backend/src/routes/schedules/validators.py
@@ -1,16 +1,6 @@
 from trafaret import Dict, Key, String, URL, Email, Bool, Float
 
 
-class CrontabValidator(Dict):
-    def __init__(self):
-        super().__init__(
-            Key('minute', default='*', trafaret=String(allow_blank=False)),
-            Key('hour', default='*', trafaret=String(allow_blank=False)),
-            Key('day_of_week', default='*', trafaret=String(allow_blank=False)),
-            Key('day_of_month', default='*', trafaret=String(allow_blank=False)),
-            Key('month_of_year', default='*', trafaret=String(allow_blank=False)))
-
-
 class MWOfflinerConfigValidator(Dict):
     def __init__(self):
         super().__init__(
