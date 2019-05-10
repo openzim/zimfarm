0ebca658c4753e0f388ab5d11ff7c45d29108cb0
ChrisLi <chrisencoded@gmail.com>
2019-05-10 09:09:15 -0400

    Update validators

--- dispatcher/backend/src/routes/schedules/validators.py
+++ dispatcher/backend/src/routes/schedules/validators.py
@@ -4,7 +4,7 @@ import trafaret as t
 mwoffliner_flags_validator = t.Dict({
     t.Key('mwUrl'): t.URL,
     t.Key('adminEmail'): t.Email,
-    t.Key('format', optional=True): t.List(t.Enum('nopic', 'novid')) >> (lambda x: list(set(x))),
+    t.Key('format', optional=True): t.List(t.Enum('nodet', 'nopic', 'novid')) >> (lambda x: list(set(x))),
     t.Key('useCache', optional=True): t.Bool,
     t.Key('verbose', optional=True): t.Bool,
     t.Key('speed', optional=True): t.Float,
@@ -12,13 +12,18 @@ mwoffliner_flags_validator = t.Dict({
     t.Key('customZimFavicon', optional=True): t.URL,
     t.Key('customZimTitle', optional=True): t.String,
     t.Key('customZimDescription', optional=True): t.String,
+    t.Key('customMainPage', optional=True): t.String,
+    t.Key('filenamePrefix', optional=True): t.String,
+    t.Key('withoutZimFullTextIndex', optional=True): t.Bool,
 })
 
 
 config_validator = t.Dict({
     t.Key('task_name'): t.Enum('offliner.mwoffliner'),
     t.Key('queue'): t.Enum('small', 'medium', 'large', 'debug'),
-    t.Key('warehouse_path'): t.Enum('/wikipedia'),
+    t.Key('warehouse_path'): t.Enum(
+        '/wikipedia', '/other', '/psiram', '/vikidia', '/wikibooks', '/wikinews',
+        '/wikiquote', '/wikisource', '/wikiversity', '/wikivoyage', '/wiktionary'),
     t.Key('image'): t.Dict(
         t.Key('name', trafaret=t.Enum('openzim/mwoffliner')),
         t.Key('tag', trafaret=t.String)),
