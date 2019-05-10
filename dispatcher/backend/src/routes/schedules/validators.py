c588b2d6816cc24b3e2f6f74b16762a19d152a00
ChrisLi <chris@kiwix.org>
2019-05-10 09:16:44 -0400

    Schedule create API validators (#200)
    * validators
    
    * test
    
    * Update validators

--- dispatcher/backend/src/routes/schedules/validators.py
+++ dispatcher/backend/src/routes/schedules/validators.py
@@ -1,22 +1,31 @@
-from trafaret import Dict, Key, String, URL, Email, Bool, Float
+import trafaret as t
 
 
-class MWOfflinerConfigValidator(Dict):
-    def __init__(self):
-        super().__init__(
-            Key('mwUrl', trafaret=URL),
-            Key('adminEmail', trafaret=Email),
-            Key('deflateTmpHtml', optional=True, trafaret=Bool),
-            Key('keepHtml', optional=True, trafaret=Bool),
-            Key('language', optional=True, trafaret=String),
-            Key('languageInverter', optional=True, trafaret=String),
-            Key('languageTrigger', optional=True, trafaret=String),
-            Key('parsoidUrl', optional=True, trafaret=URL),
-            Key('project', optional=True, trafaret=String),
-            Key('projectInverter', optional=True, trafaret=String),
-            Key('resume', optional=True, trafaret=Bool),
-            Key('speed', optional=True, trafaret=Float),
-            Key('verbose', optional=True, trafaret=Bool),
-            Key('skipHtmlCache', optional=True, trafaret=Bool),
-            Key('withZimFullTextIndex', optional=True, trafaret=Bool),
-            Key('mobileLayout', optional=True, trafaret=Bool))
+mwoffliner_flags_validator = t.Dict({
+    t.Key('mwUrl'): t.URL,
+    t.Key('adminEmail'): t.Email,
+    t.Key('format', optional=True): t.List(t.Enum('nodet', 'nopic', 'novid')) >> (lambda x: list(set(x))),
+    t.Key('useCache', optional=True): t.Bool,
+    t.Key('verbose', optional=True): t.Bool,
+    t.Key('speed', optional=True): t.Float,
+    t.Key('articleList', optional=True): t.URL,
+    t.Key('customZimFavicon', optional=True): t.URL,
+    t.Key('customZimTitle', optional=True): t.String,
+    t.Key('customZimDescription', optional=True): t.String,
+    t.Key('customMainPage', optional=True): t.String,
+    t.Key('filenamePrefix', optional=True): t.String,
+    t.Key('withoutZimFullTextIndex', optional=True): t.Bool,
+})
+
+
+config_validator = t.Dict({
+    t.Key('task_name'): t.Enum('offliner.mwoffliner'),
+    t.Key('queue'): t.Enum('small', 'medium', 'large', 'debug'),
+    t.Key('warehouse_path'): t.Enum(
+        '/wikipedia', '/other', '/psiram', '/vikidia', '/wikibooks', '/wikinews',
+        '/wikiquote', '/wikisource', '/wikiversity', '/wikivoyage', '/wiktionary'),
+    t.Key('image'): t.Dict(
+        t.Key('name', trafaret=t.Enum('openzim/mwoffliner')),
+        t.Key('tag', trafaret=t.String)),
+    t.Key('flags'): mwoffliner_flags_validator
+})
