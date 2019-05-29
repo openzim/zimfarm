9972e0d376f4c46e47e1bc0495999c6d7699abb3
renaud gaudin <rgaudin@gmail.com>
2019-05-29 03:05:01 +0000

    removed unnecessary script call in build_prod.sh

--- dispatcher/frontend/src/app/services/entities.ts
+++ dispatcher/frontend/src/app/services/entities.ts
@@ -1,5 +1,5 @@
 
-// please occasionaly sync this with backend via `gen_entities-ts.py script`
+// please occasionaly sync this with backend via `gen_entities-ts.py script` or the API
 export let queues = ['small', 'medium', 'large', 'debug'];
 export let categories = ['gutenberg', 'other', 'phet', 'psiram', 'stack_exchange', 'ted', 'vikidia', 'wikibooks', 'wikinews', 'wikipedia', 'wikiquote', 'wikisource', 'wikispecies', 'wikiversity', 'wikivoyage', 'wiktionary'];
 
