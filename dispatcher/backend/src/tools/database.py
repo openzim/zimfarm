aaf825e1dfa67f1b01ba5e5b3d7b6d0560d51f86
ChrisLi <chris.li@datarobot.com>
2019-06-03 08:47:00 -0400

    update get schedule to tun tool

--- dispatcher/backend/src/tools/database.py
+++ dispatcher/backend/src/tools/database.py
@@ -1,13 +1,14 @@
 import json
+from datetime import datetime
 
 from pymongo import MongoClient
 from sshtunnel import SSHTunnelForwarder
 
 
-def get_schedules_to_run():
+def get_schedules_to_run(query: dict):
     schedules = client['Zimfarm']['schedules']
-    schedules = schedules.find({'most_recent_task.status': {'$ne': 'succeeded'}}, {'name': 1, 'language': 1})
-    schedules = [schedule for schedule in schedules if 'f' <= schedule['language']['code'][0] <= 'g']
+    schedules = schedules.find(query, {'name': 1, 'language': 1})
+    schedules = [schedule for schedule in schedules if 'a' <= schedule['language']['code'][0] <= 'g']
     schedule_names = [schedule['name'] for schedule in schedules]
 
     print(len(schedule_names))
@@ -19,5 +20,8 @@ if __name__ == '__main__':
     with SSHTunnelForwarder('farm.openzim.org', ssh_username='chris', ssh_pkey="/Users/chrisli/.ssh/id_rsa",
                             remote_bind_address=('127.0.0.1', 27017), local_bind_address=('0.0.0.0', 27018)) as tunnel:
         with MongoClient(port=27018) as client:
-            get_schedules_to_run()
+            has_not_run_this_month = {'most_recent_task.updated_at': {'$lt': datetime(year=2019, month=6, day=1)}}
+            not_succeeded = {'most_recent_task.status': {'$ne': 'succeeded'}}
+
+            get_schedules_to_run(query=has_not_run_this_month)
     print('FINISH!')
