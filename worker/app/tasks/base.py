166b707b917c82ae62d499e01321d361eebecb1c
renaud gaudin <rgaudin@gmail.com>
2019-06-11 17:27:32 +0000

    Implement a DNS Cache to save on requests round trips
    Following openzim/mwoffliner/issues/789:
    
    * created an openzim/dnscache container based on dnsmasq
      * not zimfarm specific but currently cache domains for 1day
    * a dnscache instance is created for each task (see #234 discussion)
    * dnscache IP is retrieved
    * scrappers (mwoffliner and phet ATM) are passed dns=[<ip>] on startup
    
    * also fixed `worker_shutting_down_clean_up()` which only looked-for mwoffliner

--- worker/app/tasks/base.py
+++ worker/app/tasks/base.py
@@ -1,10 +1,13 @@
 import shutil
 from pathlib import Path
 
+import docker
 from celery.task import Task
 from celery.utils.log import get_task_logger
 
 from utils import Settings
+from utils.docker import get_ip_address
+from operations.run_dnscache import RunDNSCache
 
 logger = get_task_logger(__name__)
 
@@ -23,7 +26,31 @@ class Base(Task):
     def __init__(self):
         super().__init__()
 
+    def set_up(self):
+        # start a dnscache instance
+        try:
+            self.run_dnscache = RunDNSCache(docker_client=docker.from_env(),
+                                            task_id=self.task_id)
+            self.run_dnscache.execute()
+            logger.info(f'Running DNSCache at {self.get_dns()[-1]}')
+        except docker.errors.APIError as e:
+            raise self.retry(exc=e)
+        except (docker.errors.ContainerError, docker.errors.ImageNotFound) as e:
+            raise Exception from e
+
+    def get_dns(self):
+        """list of DNS IPs to feed `dns` on scrappers with: [<dnscache_ip>]"""
+        if not hasattr(self, 'run_dnscache'):
+            self.set_up()
+
+        return [get_ip_address(docker.from_env(), self.run_dnscache._container_name)]
+
     def clean_up(self):
+        # ensure dnscache is stopped
+        if self.run_dnscache:
+            self.run_dnscache.stop()
+
+        # remove working_dir
         working_dir = Path(Settings.working_dir_container).joinpath(self.task_id)
         shutil.rmtree(working_dir)
 
