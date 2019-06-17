2f07362cf276f9fc793f76667506ff41711006eb
ChrisLi <chris@kiwix.org>
2019-06-17 19:10:10 -0400

    Update schedule UI (#241)
    * Angular update
    
    * clean up and add search
    
    * add search
    
    * add search
    
    * add search
    
    * schedule list css
    
    * style
    
    * schedule detail
    
    * List
    
    * Update types
    
    * rebuild
    
    * Rebuild frontend

---
+++ dispatcher/frontend/src/app/schedule/schedule-list/schedule-list.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Schedule, SchedulesService, SchedulesListRequestParams } from 'src/app/services/schedules.service';
+
+import { LanguagesService } from '../../services/languages.service';
+
+@Component({
+    templateUrl: './schedule-list.html',
+    styleUrls: ['./schedule-list.css']
+})
+export class ScheduleListComponent implements OnInit {
+    scheduleFilterForm: FormGroup;
+    schedules: Array<Schedule> = []
+
+    constructor(
+        private router: Router, 
+        private languagesService: LanguagesService,
+        private schedulesService: SchedulesService, 
+        private formBuilder: FormBuilder) {
+            this.scheduleFilterForm = this.formBuilder.group({
+                name: '',
+            })
+        }
+
+    ngOnInit() {
+        let params = new SchedulesListRequestParams()
+        params.limit = 200
+        this.schedulesService.list(params).subscribe(data => {
+            this.schedules = data.items;
+        })
+        this.scheduleFilterForm.valueChanges.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            switchMap(value => {
+                let params = new SchedulesListRequestParams()
+                params.limit = 200
+                params.name = value['name']
+                return this.schedulesService.list(params)
+            })
+        ).subscribe(data => {
+            this.schedules = data.items;
+        })
+    }
+
+    clearSearchText() {
+        this.scheduleFilterForm.controls['name'].setValue('')
+    }
+
+    scheduleListScrollIndexChange(event) {
+
+    }
+}
