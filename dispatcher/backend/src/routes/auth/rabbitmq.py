8411a2a6b4ae8ee76bed6b2740ba229db55c528d
rgaudin <rgaudin@gmail.com>
2019-07-06 09:00:25 +0000

    Cancel started Task (#255)
    * fixed rabbit auth is username is wrong: return deny instead of exc
    
    * added cancel endpoint
    
    POST `/tasks/<tid>/cancel`
    
    * added revoke handler to stop and remove dnscache container
    
    * added tests for cancel endpoint
    
    * removed duplicated test

--- dispatcher/backend/src/routes/auth/rabbitmq.py
+++ dispatcher/backend/src/routes/auth/rabbitmq.py
@@ -29,9 +29,11 @@ def auth(intention: str):
             return Response('allow')
         else:
             user = Users().find_one({'username': username}, {'_id': 0, 'password_hash': 1, 'scope.rabbitmq': 1})
+            if user is None:
+                return Response('deny')
             password_hash = user.get('password_hash', '')
             tags = user.get('scope', {}).get('rabbitmq', [])
-            if user is not None and check_password_hash(password_hash, password):
+            if check_password_hash(password_hash, password):
                 tags = ['allow'] + tags
                 return Response(' '.join(tags))
             else:
