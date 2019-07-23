dd1c27a3284e1d3c9d4c8325cb0d085c5b4d87c2
renaud gaudin <rgaudin@gmail.com>
2019-07-23 14:58:50 +0200

    gaining coverage

--- dispatcher/backend/src/routes/schedules/schedule.py
+++ dispatcher/backend/src/routes/schedules/schedule.py
@@ -99,13 +99,6 @@ class SchedulesRoute(BaseRoute):
 
         return Response(json.dumps({'_id': str(schedule_id)}), HTTPStatus.CREATED)
 
-    @authenticate
-    def delete(self):
-        return jsonify({})
-
-    def toto(self):
-        pass
-
 
 class SchedulesBackupRoute(BaseRoute):
     rule = '/backup/'
@@ -124,7 +117,7 @@ class SchedulesBackupRoute(BaseRoute):
 class ScheduleRoute(BaseRoute, ScheduleQueryMixin):
     rule = '/<string:schedule>'
     name = 'schedule'
-    methods = ['GET', 'PATCH', 'PUT', 'DELETE']
+    methods = ['GET', 'PATCH', 'DELETE']
 
     @authenticate
     def get(self, schedule: str, *args, **kwargs):
@@ -136,10 +129,6 @@ class ScheduleRoute(BaseRoute, ScheduleQueryMixin):
             raise ScheduleNotFound()
         else:
             return jsonify(schedule)
-
-    @authenticate
-    def put(self):
-        return jsonify({})
 
     @authenticate
     def patch(self, schedule: str, *args, **kwargs):
