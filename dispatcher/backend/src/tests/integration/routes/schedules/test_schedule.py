27f8b06ae8dd1f0f26699555c32e993ae9bbbc46
renaud gaudin <rgaudin@gmail.com>
2019-08-16 11:03:09 +0200

    added tests for gutenberg values in validator

--- dispatcher/backend/src/tests/integration/routes/schedules/test_schedule.py
+++ dispatcher/backend/src/tests/integration/routes/schedules/test_schedule.py
@@ -26,6 +26,13 @@ good_patch_updates = [
         'flags': {},
         'image': {'name': 'openzim/phet', 'tag': 'latest'},
     }},
+    {'config': {
+        'task_name': 'offliner.gutenberg',
+        'queue': 'large',
+        'warehouse_path': '/gutenberg',
+        'flags': {},
+        'image': {'name': 'openzim/gutenberg', 'tag': 'latest'},
+    }},
     {'name': 'new_name'},
 ]
 
@@ -76,6 +83,13 @@ bad_patch_updates = [
         'warehouse_path': '/phet',
         'flags': {},
         'image': {'name': 'openzim/youtube', 'tag': 'latest'},
+    }},
+    {'config': {
+        'task_name': 'offliner.gutenberg',
+        'queue': 'small',
+        'warehouse_path': '/gutenberg',
+        'flags': {},
+        'image': {'name': 'openzim/youtube', 'tag': 'latest'},
     }}
 ]
 
@@ -102,7 +116,7 @@ class TestScheduleList:
             assert isinstance(item['tags'], list)
 
     @pytest.mark.parametrize('skip, limit, expected', [
-        (0, 30, 30), (10, 15, 15), (40, 25, 10), (100, 100, 0), ('', 10, 10), (5, 'abc', 20)
+        (0, 30, 30), (10, 15, 15), (40, 25, 11), (100, 100, 0), ('', 10, 10), (5, 'abc', 20)
     ])
     def test_list_schedules_with_param(self, client, access_token, schedules, skip, limit, expected):
 
@@ -122,14 +136,16 @@ class TestScheduleList:
         ("lang=bm", 1),
         ("lang=bm&lang=fr", 3),
         ("category=phet", 1),
+        ("category=gutenberg", 1),
         ("category=wikibooks", 1),
         ("category=wikipedia", 47),
         ("category=phet&category=wikipedia", 48),
+        ("category=gutenberg&category=phet&category=wikipedia", 49),
         ("tag=all", 2),
         ("tag=mini", 2),
         ("tag=all&tag=mini", 1),
         ("queue=small", 2),
-        ("queue=offliner_default", 48),
+        ("queue=offliner_default", 49),
         ("queue=offliner_default&queue=small", 50),
         ("name=youtube&lang=fr&category=other&tag=nopic&tag=novid&queue=small", 1),
     ])
@@ -152,8 +168,8 @@ class TestScheduleList:
 
 class TestSchedulePost:
 
-    def test_create_schedule(self, database, client, access_token):
-        schedule = {
+    @pytest.mark.parametrize('document', [
+        {
             "name": "wikipedia_bm_test",
             "category": "wikipedia",
             "enabled": False,
@@ -170,22 +186,41 @@ class TestSchedulePost:
                 "flags": {},
                 "image": {"name": "openzim/phet", "tag": "latest"},
             },
-        }
+        },
+        {
+            "name": "gutenberg_multi",
+            "category": "gutenberg",
+            "enabled": True,
+            "tags": ["full", "multi"],
+            "language": {
+                "code": "multi",
+                "name_en": "Multiple Languages",
+                "name_native": "Multiple Languages",
+            },
+            "config": {
+                "task_name": "offliner.gutenberg",
+                "queue": "debug",
+                "warehouse_path": "/gutenberg",
+                "flags": {},
+                "image": {"name": "openzim/gutenberg", "tag": "latest"},
+            },
+        }])
+    def test_create_schedule(self, database, client, access_token, document):
 
         url = '/api/schedules/'
-        response = client.post(url, json=schedule, headers={'Authorization': access_token})
+        response = client.post(url, json=document, headers={'Authorization': access_token})
         assert response.status_code == 201
 
-        url = '/api/schedules/{}'.format(schedule['name'])
+        url = '/api/schedules/{}'.format(document['name'])
         response = client.get(url, headers={'Authorization': access_token})
         assert response.status_code == 200
 
         response_json = response.get_json()
-        schedule['_id'] = response_json['_id']
-        assert response.get_json() == schedule
+        document['_id'] = response_json['_id']
+        assert response.get_json() == document
 
         # remove from DB to prevent count mismatch on other tests
-        database.schedules.delete_one({'_id': ObjectId(schedule['_id'])})
+        database.schedules.delete_one({'_id': ObjectId(document['_id'])})
 
     @pytest.mark.parametrize('key', ['name', 'category', 'enabled', 'tags', 'language', 'config'])
     def test_create_schedule_missing_keys(self, client, access_token, key):
