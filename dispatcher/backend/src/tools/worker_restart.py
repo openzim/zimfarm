a17ea12806f36330152995e61b747f5cec4772cb
renaud gaudin <rgaudin@gmail.com>
2019-11-13 17:43:10 +0000

    removed orphan code

--- dispatcher/backend/src/tools/worker_restart.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-username = getenv('ZIMFARM_USERNAME')
-username = getenv('ZIMFARM_PASSWORD')
-username = getenv('ZIMFARM_WORKING_DIR')
-username = getenv('ZIMFARM_NODE_NAME')
-username = getenv('ZIMFARM_QUEUES')
-username = getenv('ZIMFARM_CONCURRENCY')
