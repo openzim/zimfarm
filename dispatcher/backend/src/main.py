ed48eece6dec0910b9ae333e97e941cf3652ff75
renaud gaudin <rgaudin@gmail.com>
2019-11-21 21:49:23 +0000

    adding OpenAPI spec and swagger-ui

--- dispatcher/backend/src/main.py
+++ dispatcher/backend/src/main.py
@@ -1,10 +1,12 @@
 import os
+import pathlib
 import logging
 
-from flask import Flask, Response
+from flask import Flask, render_template, Response, make_response, redirect
 from flask_cors import CORS
 
 from routes import (
+    API_PATH,
     auth,
     schedules,
     users,
@@ -19,8 +21,12 @@ from utils.json import Encoder
 from utils.database import Initializer
 from utils.broadcaster import BROADCASTER
 
-
-app = Flask(__name__)
+if os.getenv("DOCS_DIR"):
+    docs_dir = pathlib.Path(os.getenv("DOCS_DIR")).resolve()
+else:
+    # docs dir outside codebase
+    docs_dir = pathlib.Path(__file__).parent.resolve().parent.joinpath("docs")
+app = Flask(__name__, template_folder=docs_dir)
 app.json_encoder = Encoder
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -32,9 +38,27 @@ handler.setFormatter(logging.Formatter("[%(asctime)s: %(levelname)s] %(message)s
 logger.addHandler(handler)
 
 
+@app.route(f"{API_PATH}/openapi.yaml")
+def openapi():
+    fname = "openapi_v1.yaml"
+    if not docs_dir.joinpath(fname).exists():
+        return Response("Zimfarm API (OpenAPI spec missing)")
+    resp = make_response(render_template(fname))
+    resp.headers["Content-Type"] = "application/yaml"
+    return resp
+
+
+@app.route(f"{API_PATH}")
+def api_doc():
+    fname = "swagger-ui.html"
+    if not docs_dir.joinpath(fname).exists():
+        return Response("Zimfarm API (documentation missing)")
+    return render_template(fname)
+
+
 @app.route("/")
 def home():
-    return Response("zimfarm API")
+    return redirect("/v1")
 
 
 app.register_blueprint(auth.Blueprint())
