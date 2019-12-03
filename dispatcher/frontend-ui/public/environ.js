5364fbac1cdc18f79ea560589ec2b0298ef7dba2
renaud gaudin <rgaudin@gmail.com>
2019-12-03 17:40:50 +0000

    disabling no-unused-vars for environ file

--- dispatcher/frontend-ui/public/environ.js
+++ dispatcher/frontend-ui/public/environ.js
@@ -1,4 +1,4 @@
-/*eslint no-unused-vars: ["error", { "vars": "local" }]*/
-/* global environ */
+/* eslint no-unused-vars: 0 */
+/* exported environ */
 
 var environ = {"ZIMFARM_WEBAPI": "https://api.farm.openzim.org/v1"};
