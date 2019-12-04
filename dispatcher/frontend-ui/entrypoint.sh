64585609ffe6fa4c2d913d3ec37e635404a32e36
renaud gaudin <rgaudin@gmail.com>
2019-12-04 11:58:02 +0000

    made entrypoint bourne-shell compatible

--- dispatcher/frontend-ui/entrypoint.sh
+++ dispatcher/frontend-ui/entrypoint.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 JS_PATH=/usr/share/nginx/html/environ.js
 echo "dump ZIMFARM_* environ variables to $JS_PATH"
 
-json_output="environ = {\n"
+json_output="var environ = {\n"
 envs_output=$(env)
-envLines=(${envs_output/\\n/ })
-for envLine in "${envLines[@]}"; do
-	lineElems=(${envLine/=/ })
-	key=${lineElems[0]}
-	if [[ "$key" =~ ^ZIMFARM_.+$ ]]; then
-		json_output+="    \"$key\": \"${lineElems[1]}\",\n"
-	fi
+envLines=${envs_output//;/$'\n'}
+for envLine in $envLines
+do
+    if [ "$(echo "$envLine" | cut -c0-8)" == "ZIMFARM_" ]; then
+        echo $envLine
+        ns=$(echo $envLine | sed 's/=/": "/')
+        json_output="$json_output    \"$ns\",\n"
+    fi
 done
-json_output+="}\n"
+json_output="$json_output}\n"
 echo -e $json_output > $JS_PATH
 
 cat $JS_PATH
