8efc6df194f4a1976281528af44e8945b78c018c
renaud gaudin <rgaudin@gmail.com>
2019-12-05 08:44:41 +0000

    backbutton only show if we have history

--- dispatcher/frontend-ui/src/components/BackButton.vue
+++ dispatcher/frontend-ui/src/components/BackButton.vue
@@ -1,14 +1,19 @@
 <!-- Simple back buttton to mimick browser back -->
 <template>
-  <button class="btn btn-secondary btn-sm" @click.prevent="goBack"><font-awesome-icon icon="arrow-circle-left" size="sm" /> Go Back</button>
+  <button class="btn btn-secondary btn-sm" @click.prevent="goBack" v-show="has_history">
+    <font-awesome-icon icon="arrow-circle-left" size="sm" /> Go Back
+  </button>
 </template>
 
 <script type="text/javascript">
   export default {
     name: 'BackButton',
+    computed: {
+      has_history() { return window.history.length > 1; }
+    },
     methods: {
       goBack: function() {
-        this.$router.go(-1);
+        this.$router.back();
       }
     }
   }
