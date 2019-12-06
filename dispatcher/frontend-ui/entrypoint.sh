cf68223d963838af6d2f8d63de06adf0264be3da
renaud gaudin <rgaudin@gmail.com>
2019-12-06 12:10:06 +0000

    minor tweaks to entrypoint for codefactor

--- dispatcher/frontend-ui/entrypoint.sh
+++ dispatcher/frontend-ui/entrypoint.sh
@@ -5,17 +5,18 @@ echo "dump ZIMFARM_* environ variables to $JS_PATH"
 
 json_output="var environ = {\n"
 envs_output=$(env)
+# shellcheck disable=SC2039
 envLines=${envs_output//;/$'\n'}
 for envLine in $envLines
 do
-    if [ "$(echo "$envLine" | cut -c0-8)" == "ZIMFARM_" ]; then
-        echo $envLine
-        ns=$(echo $envLine | sed 's/=/": "/')
+    if [ "$(echo "$envLine" | cut -c0-8)" = "ZIMFARM_" ]; then
+        ns=$(echo "$envLine" | sed 's/=/": "/')
         json_output="$json_output    \"$ns\",\n"
     fi
 done
 json_output="$json_output}\n"
-echo -e $json_output > $JS_PATH
+# shellcheck disable=SC2039
+echo -e "$json_output" > $JS_PATH
 
 cat $JS_PATH
 echo "-----"
