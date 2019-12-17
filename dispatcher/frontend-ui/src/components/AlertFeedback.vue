5b5671425a2c6922d84b56c3f6d9a0f3148e9328
renaud gaudin <rgaudin@gmail.com>
2019-12-17 11:03:49 +0000

    alert now relative and disapearing after 5s

--- dispatcher/frontend-ui/src/components/AlertFeedback.vue
+++ dispatcher/frontend-ui/src/components/AlertFeedback.vue
@@ -2,10 +2,14 @@
 
 <template>
   <div class="container">
-    <div class="col-xs-12 col-sm-8 col-md-6 col-lg-5 alertbox alert" :class="alertClass" role="alert" v-if="message">
-      <font-awesome-icon class="dismiss" icon="times" size="sm" @click="dismiss" />
+    <b-alert v-if="message"
+             :show="dismissCountDown"
+             dismissible
+             fade
+             :variant="level"
+             @dismiss-count-down="countDownChanged">
       <div v-html="message"></div>
-    </div>
+    </b-alert>
   </div>
 </template>
 
@@ -16,57 +20,20 @@
       return {
         level: null,  // bootstrap-alert-like class to use: info, success, warning, danger
         message: null,  // message to display
+        dismissCountDown: 0,  // live-updating counter
       };
     },
-    computed: { // actual bootstrap class from level
-      alertClass() { return 'alert-' + this.level; }
-    },
     methods: {
-      dismiss() { // remove alert
-        this.level = this.message = null;
-      },
+      countDownChanged(dismissCountDown) { this.dismissCountDown = dismissCountDown; },
     },
     beforeMount(){
       let parent = this;
       this.$root.$on('feedback-message', function (level, message) {
         parent.level = level;
         parent.message = message;
+        parent.dismissCountDown = 5;
       });
     }
   }
 </script>
 
-<style type="text/css" scoped>
-  .dismiss {
-    position: absolute;
-    top: .25rem;
-    right: .25rem;
-  }
-  .dismiss:hover {
-    cursor: pointer;
-    color: #333333;
-  }
-
-  .container {
-    position: relative;
-  }
-  .alertbox {
-    margin-left:auto;
-    margin-right: 0;
-
-    position: fixed;
-    z-index: 900;
-
-    width: auto;
-    right: .75rem;
-    box-sizing: border-box;
-    padding: .8rem;
-  }
-  @media (max-width: 575.98px) {
-    .alertbox {
-      border-radius: 0;
-      right: 0;
-      width: 100%;
-    }
-  }
-</style>
