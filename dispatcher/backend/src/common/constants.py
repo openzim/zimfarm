ff7666dbf1619db7b7c11d07b9bf1f0347bb74d5
renaud gaudin <rgaudin@gmail.com>
2020-01-07 14:39:54 +0000

    refresh token to last 6 months (kelson)

--- dispatcher/backend/src/common/constants.py
+++ dispatcher/backend/src/common/constants.py
@@ -3,5 +3,5 @@ import os
 OPENSSL_BIN = os.getenv("OPENSSL_BIN", "/usr/bin/openssl")
 MESSAGE_VALIDITY = 60  # number of seconds before a message expire
 
-REFRESH_TOKEN_EXPIRY = 30  # days
+REFRESH_TOKEN_EXPIRY = 180  # days
 TOKEN_EXPIRY = 24  # hours
