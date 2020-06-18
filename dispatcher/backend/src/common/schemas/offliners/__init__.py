23d002f5d7a21546b02b1d5b709f1d23b136b2a2
renaud gaudin <rgaudin@gmail.com>
2020-06-18 12:26:10 +0000

    Add more flexible gutenberg support
    Now that gutenberg supports S3 cache, we need to be able to configure it from zimfarm
    Also, new features like bokshelves and title search might lead to additional recipes
    zimfarm now supports all gutenberg options, allowing custom recipes along out main one

--- dispatcher/backend/src/common/schemas/offliners/__init__.py
+++ dispatcher/backend/src/common/schemas/offliners/__init__.py
@@ -4,13 +4,10 @@ from common.schemas.offliners.youtube import YoutubeFlagsSchema
 from common.schemas.offliners.sotoki import SotokiFlagsSchema
 from common.schemas.offliners.nautilus import NautilusFlagsSchema
 from common.schemas.offliners.ted import TedFlagsSchema
+from common.schemas.offliners.gutenberg import GutenbergFlagsSchema
 
 
 class PhetFlagsSchema(SerializableSchema):
     class Meta:
         ordered = True
 
-
-class GutenbergFlagsSchema(SerializableSchema):
-    class Meta:
-        ordered = True
