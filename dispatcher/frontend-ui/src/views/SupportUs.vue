0b27a1a475c508ce3d5a25e1ab6a4ea0b0ea8e3d
renaud gaudin <rgaudin@gmail.com>
2020-07-16 08:13:32 +0000

    new support-us oriented wording

--- dispatcher/frontend-ui/src/views/SupportUs.vue
+++ dispatcher/frontend-ui/src/views/SupportUs.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container about">
-    <h2><img :src="publicPath + 'assets/logo.svg'" />About the the Zimfarm</h2>
-    <p>The <strong>Zimfarm</strong> is <a target="_blank" href="https://kiwix.org">Kiwix</a> and <a target="_blank" href="https://openzim.org">openZIM</a>'s platform for automating the creation and re-creation (updates) of <a target="_blank" href="https://wiki.openzim.org/wiki/ZIM_File_Format">ZIM files</a>.</p>
-    <p>Our catalog is composed of a thousand <router-link :to="{name: 'schedules-list'}">recipes</router-link> which usually creates & publishes several flavoured <a target="_blank" href="https://download.kiwix.org/zim/">ZIM files</a> each. ZIM files are built by a selection of distributed <router-link :to="{name: 'workers'}">workers</router-link>.</p>
+    <h2>Support Zimfarm <img :src="publicPath + 'assets/logo.svg'" /></h2>
 
-    <p>Help us build better ZIM files quicker by providing us a worker. If you have a GNU/Linux system with at least 4 cores and 2GB of RAM, it's easy. A good connection to Internet with a fix IP or stable IP range is necessary too. You just have to follow <a target="_blank" href="https://github.com/openzim/zimfarm/blob/master/workers/README.md">these instructions</a>. If you have a barbone server you want to give us access to, <a href="mailto:contact+zimfarm@kiwix.org">it's even easier</a>!</p>
+    <p>Zimfarm is <a target="_blank" href="https://kiwix.org">Kiwix</a>’ platform for automating the creation and update of <a target="_blank" href="https://wiki.openzim.org/wiki/ZIM_File_Format">ZIM files</a>. Thousands or recipes are run by distributed <router-link :to="{name: 'workers'}">workers</router-link>. All day, every day, so you can enjoy offline content.</p>
 
-    <p>Zimfarm is a Python project hosted <a target="_blank" href="https://github.com/openzim/zimfarm">on Github</a>. It is built on top of content-specific scrapers like <code>mwoffliner</code>, <code>youtube</code>, <code>sotoki</code>, and others. Check <a target="_blank" href="https://github.com/openzim/zimfarm/blob/master/dispatcher/backend/src/common/schemas/offliners/__init__.py">OpenZIM's repos</a> for a complete list.</p>
+    <p>It is <strong>entirely and only supported by donations</strong>. A few dollars may not mean much for you but will certainly help us a lot : if you can, <a target="_blank" href="https://support.kiwix.org/">consider supporting us</a> - even with the price of a cup of coffee (we love coffee and at times need quite a bit of it).</p>
+
+    <p>Another way to help is by <strong>donating a worker</strong> : if you have a GNU/Linux system with at least 4 cores, 2GB of RAM and a fixed public IP, <a target="_blank" href="https://github.com/openzim/zimfarm/blob/master/workers/README.md">follow these instructions</a>. And if you have a headless server you want to give us access to, <a href="mailto:contact+zimfarm@kiwix.org">it's even easier</a>!</p>
+
+    <p>Zimfarm is a Python project hosted <a target="_blank" href="https://github.com/openzim/zimfarm">on Github</a>. It is built on top of content-specific scrapers like <code>mwoffliner</code>, <code>youtube</code>, <code>sotoki</code>, and others. Check out <a target="_blank" href="https://github.com/openzim/zimfarm/blob/master/dispatcher/backend/src/common/schemas/offliners/__init__.py">OpenZIM's repositories</a> for a complete list.</p>
   </div>
 </template>
 
