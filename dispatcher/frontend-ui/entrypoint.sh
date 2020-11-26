d1b6a08b05617e841844b8f5ab05d600073696bd
renaud gaudin <rgaudin@gmail.com>
2020-11-26 15:38:08 +0000

    install python in ui image for entrypoint

--- dispatcher/frontend-ui/entrypoint.sh
+++ dispatcher/frontend-ui/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 JS_PATH=/usr/share/nginx/html/environ.json
-echo "dump ZIM* environ variables to $JS_PATH"
+echo "dump ZIMFARM_* environ variables to $JS_PATH"
 
-python -c 'import os; import json; print(json.dumps({k: v for k, v in os.environ.items() if k.startswith("ZIMFARM_")}, indent=4))' > $JS_PATH
+python3 -c 'import os; import json; print(json.dumps({k: v for k, v in os.environ.items() if k.startswith("ZIMFARM_")}, indent=4))' > $JS_PATH
 
 cat $JS_PATH
 echo "-----"
