0d481db7ba02ba3ec5443230d957b4eb6b18de20
renaud gaudin <rgaudin@gmail.com>
2020-12-29 13:10:48 +0000

    Use x_UPLOAD_URI to specify upload path
    `ZIM_UPLOAD_URI` and `LOGS_UPLOAD_URI` were specified as scheme://netloc and the worker
    used to hard-code a *folder* (`zim/` or `logs/`) at upload time to place file into.
    This was inherited by previous warehouse-based upload to ensure we're uploading
    only to safe places.
    This is not flexible enough as it forces path on server for stuff that shouldn't require it.
    Full path is now specified on the ENV so we moved from.

--- dispatcher/backend/src/common/constants.py
+++ dispatcher/backend/src/common/constants.py
@@ -19,21 +19,21 @@ PERIODICITIES = {
 }
 
 ZIM_UPLOAD_URI = os.getenv(
-    "ZIM_UPLOAD_URI", "sftp://uploader@warehouse.farm.openzim.org:1522"
+    "ZIM_UPLOAD_URI", "sftp://uploader@warehouse.farm.openzim.org:1522/zim"
 )
 try:
     ZIM_EXPIRATION = int(os.getenv("ZIM_EXPIRATION", "0"))
 except Exception:
     ZIM_EXPIRATION = 0
 LOGS_UPLOAD_URI = os.getenv(
-    "LOGS_UPLOAD_URI", "sftp://uploader@warehouse.farm.openzim.org:1522"
+    "LOGS_UPLOAD_URI", "sftp://uploader@warehouse.farm.openzim.org:1522/logs"
 )
 try:
     LOGS_EXPIRATION = int(os.getenv("LOGS_EXPIRATION", "30"))
 except Exception:
     LOGS_EXPIRATION = 30
 
-### NOTIFICATIONS
+# NOTIFICATIONS
 
 # in-notification URLs
 PUBLIC_URL = os.getenv("PUBLIC_URL", "https://farm.openzim.org")
