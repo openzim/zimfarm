d336baa2c9918e0c3761e1c63fa4a79430cae4ea
renaud gaudin <rgaudin@gmail.com>
2021-01-11 15:18:44 +0000

    Replaced openzim/checker with openzim/zim-tools image
    - in zimfarm.sh env
    - in workers constants
    
    Also tweaked zimfarm.sh a bit:
    - allowed use of local images (using no tag)
    - changed config to configfile and now displaying config content on config

--- workers/app/common/constants.py
+++ workers/app/common/constants.py
@@ -20,7 +20,7 @@ TASK_WORKER = "task-worker"
 TASK_WORKER_IMAGE = os.getenv("TASK_WORKER_IMAGE", "openzim/zimfarm-task-worker:latest")
 DNSCACHE_IMAGE = os.getenv("DNSCACHE_IMAGE", "openzim/dnscache:1.0")
 UPLOADER_IMAGE = os.getenv("UPLOADER_IMAGE", "openzim/uploader:1.1")
-CHECKER_IMAGE = os.getenv("CHECKER_IMAGE", "openzim/checker:1.0.1")
+CHECKER_IMAGE = os.getenv("CHECKER_IMAGE", "openzim/zim-tools:2.1.0")
 
 # paths
 DEFAULT_WORKDIR = os.getenv("WORKDIR", "/data")  # in-container workdir for manager
