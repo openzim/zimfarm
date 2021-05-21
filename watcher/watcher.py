11198a56022452393ebc647089e7aa47183c7479
renaud gaudin <rgaudin@gmail.com>
2021-05-21 21:25:44 +0000

    cast nb_threads to int

--- watcher/watcher.py
+++ watcher/watcher.py
@@ -519,6 +519,7 @@ def entrypoint():
         help="How many threads to run to parallelize download/upload? "
         "Defaults to 1 inside Docker as we can't guess available CPUs",
         dest="nb_threads",
+        type=int,
         default=1
         if is_running_inside_container()
         else multiprocessing.cpu_count() - 1 or 1,
