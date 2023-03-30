1998a8e733b6e92e65f7eb03b860f4fc64e0401d
Benoit BERAUD <benoit74@users.noreply.github.com>
2023-03-30 18:05:57 +0200

    F401: unused imports should be place in __all__ to not raise issue

--- dispatcher/backend/src/common/schemas/offliners/__init__.py
+++ dispatcher/backend/src/common/schemas/offliners/__init__.py
@@ -1,4 +1,29 @@
 from common.schemas import SerializableSchema
+from common.schemas.offliners.gutenberg import GutenbergFlagsSchema
+from common.schemas.offliners.ifixit import IFixitFlagsSchema
+from common.schemas.offliners.kolibri import KolibriFlagsSchema
+from common.schemas.offliners.mwoffliner import MWOfflinerFlagsSchema
+from common.schemas.offliners.nautilus import NautilusFlagsSchema
+from common.schemas.offliners.openedx import OpenedxFlagsSchema
+from common.schemas.offliners.sotoki import SotokiFlagsSchema
+from common.schemas.offliners.ted import TedFlagsSchema
+from common.schemas.offliners.wikihow import WikihowFlagsSchema
+from common.schemas.offliners.youtube import YoutubeFlagsSchema
+from common.schemas.offliners.zimit import ZimitFlagsSchema
+
+__all__ = (
+    "GutenbergFlagsSchema",
+    "IFixitFlagsSchema",
+    "KolibriFlagsSchema",
+    "MWOfflinerFlagsSchema",
+    "NautilusFlagsSchema",
+    "OpenedxFlagsSchema",
+    "SotokiFlagsSchema",
+    "TedFlagsSchema",
+    "WikihowFlagsSchema",
+    "YoutubeFlagsSchema",
+    "ZimitFlagsSchema",
+)
 
 
 class PhetFlagsSchema(SerializableSchema):
