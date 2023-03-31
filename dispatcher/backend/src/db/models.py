fc15a10d1c2e0efa3e88dc9da40ca9d70c3a1883
Benoit BERAUD <benoit74@users.noreply.github.com>
2023-03-31 15:20:57 +0200

    First small use of SLQA for listing users

--- dispatcher/backend/src/db/models.py
+++ dispatcher/backend/src/db/models.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional
 
 from sqlalchemy import DateTime, ForeignKey, String, text
 from sqlalchemy.dialects.postgresql import JSONB
@@ -14,7 +14,7 @@ from sqlalchemy.orm import (
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
-    type_annotation_map = {dict[str, Any]: JSONB, datetime: DateTime(timezone=True)}
+    type_annotation_map = {Dict[str, Any]: JSONB, datetime: DateTime(timezone=True)}
     pass
 
 
@@ -23,11 +23,11 @@ class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         init=False, primary_key=True, server_default=text("uuid_generate_v4()")
     )
-    mongo_val: Mapped[Optional[dict[str, Any]]]
+    mongo_val: Mapped[Optional[Dict[str, Any]]]
     username: Mapped[Optional[str]]
     password_hash: Mapped[Optional[str]]
     email: Mapped[Optional[str]]
-    scope: Mapped[Optional[dict[str, Any]]]
+    scope: Mapped[Optional[Dict[str, Any]]]
 
     ssh_keys: Mapped[List["Sshkey"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
@@ -39,7 +39,7 @@ class Sshkey(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         init=False, primary_key=True, server_default=text("uuid_generate_v4()")
     )
-    mongo_val: Mapped[Optional[dict[str, Any]]]
+    mongo_val: Mapped[Optional[Dict[str, Any]]]
     name: Mapped[Optional[str]]
     fingerprint: Mapped[Optional[str]]
     type: Mapped[Optional[str]]
