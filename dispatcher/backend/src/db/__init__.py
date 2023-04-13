17ca09da2e2d769a25d3fddc29a254b0f35fee8f
Benoit BERAUD <benoit74@users.noreply.github.com>
2023-04-13 07:32:15 +0200

    Add a hack to not initialize the DB when not needed indeed

--- dispatcher/backend/src/db/__init__.py
+++ dispatcher/backend/src/db/__init__.py
@@ -6,7 +6,12 @@ from sqlalchemy.orm import sessionmaker
 if not os.getenv("POSTGRES_URI"):
     raise EnvironmentError("Please set the POSTGRES_URI environment variable")
 
-Session = sessionmaker(bind=create_engine(os.getenv("POSTGRES_URI"), echo=False))
+if (
+    os.getenv("POSTGRES_URI") == "nodb"
+):  # this is a hack for cases where we do not need the DB, e.g. unit tests
+    Session = None
+else:
+    Session = sessionmaker(bind=create_engine(os.getenv("POSTGRES_URI"), echo=False))
 
 
 def dbsession(func):
