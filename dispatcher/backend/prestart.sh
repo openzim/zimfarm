c413bba86e8d81c30c1409a93758b2420c725ecf
renaud gaudin <reg@rskg.org>
2023-06-05 13:27:59 +0000

    Added ability to run alembic upgrade on start (via ENV)

--- dispatcher/backend/prestart.sh
+++ dispatcher/backend/prestart.sh
@@ -5,4 +5,10 @@ die() {
 	exit 1
 }
 
+if [ ! -z "$ALEMBIC_UPGRADE_HEAD_ON_START" ]; then
+    echo "Running alembic upgrade"
+    alembic check
+    alembic history
+    alembic upgrade head
+fi
 python -c "from utils.database import Initializer; Initializer.create_initial_user()" || die
