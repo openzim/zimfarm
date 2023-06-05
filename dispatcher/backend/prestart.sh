b618bea67d6cc5b05bceb09711dd2632faeb00e0
renaud gaudin <reg@rskg.org>
2023-06-05 13:36:57 +0000

    don't fail on alembic check (that's the point)

--- dispatcher/backend/prestart.sh
+++ dispatcher/backend/prestart.sh
@@ -7,7 +7,7 @@ die() {
 
 if [ ! -z "$ALEMBIC_UPGRADE_HEAD_ON_START" ]; then
     echo "Running alembic upgrade"
-    alembic check
+    alembic check || true
     alembic history
     alembic upgrade head
 fi
