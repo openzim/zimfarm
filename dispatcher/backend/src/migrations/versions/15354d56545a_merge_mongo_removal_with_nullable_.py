f571b6f8e69af2678b598c6add5b0acf7cbec166
renaud gaudin <reg@rskg.org>
2023-06-05 09:25:19 +0000

    added merge migration

---
+++ dispatcher/backend/src/migrations/versions/15354d56545a_merge_mongo_removal_with_nullable_.py
@@ -0,0 +1,23 @@
+"""merge mongo-removal with nullable schedule id
+
+Revision ID: 15354d56545a
+Revises: 3c4d546fde7c, 62227054989d
+Create Date: 2023-06-05 09:22:07.513517
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "15354d56545a"
+down_revision = ("3c4d546fde7c", "62227054989d")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
