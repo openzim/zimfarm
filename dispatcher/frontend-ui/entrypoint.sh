592c2b5701c7a5f1c7a83bd21c803c8886e07e52
benoit74 <benoit74@users.noreply.github.com>
2023-09-27 08:19:10 +0200

    ENVIRON_PATH is a mandatory environment variable + small fixes

--- dispatcher/frontend-ui/entrypoint.sh
+++ dispatcher/frontend-ui/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -z "${ENVIRON_PATH}" ]
-then
-    ENVIRON_PATH=/usr/share/nginx/html/environ.json
-fi
 echo "dump ZIMFARM_* environ variables to $ENVIRON_PATH"
 
 python3 -c 'import os; import json; print(json.dumps({k: v for k, v in os.environ.items() if k.startswith("ZIMFARM_")}, indent=4))' > $ENVIRON_PATH
