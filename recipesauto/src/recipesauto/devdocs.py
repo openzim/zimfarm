e59cc812d8f1719b6efc2601c870e49e04146480
benoit74 <benoit74@users.noreply.github.com>
2025-01-09 15:42:55 +0000

    Fix devdocs recipes to match ZIM naming convention and find proper icons

--- recipesauto/src/recipesauto/devdocs.py
+++ recipesauto/src/recipesauto/devdocs.py
@@ -30,12 +30,12 @@ def get_expected_recipes() -> list[dict[str, Any]]:
             "category": "devdocs",
             "config": {
                 "flags": {
-                    "logo-format": f'https://drive.farm.openzim.org/devdocs/{_get_slug_with_version(item["slug"])}.png',
+                    "logo-format": (_get_icon_url_for_slug(item["slug"])),
                     "output": "/output",
                     "slug": item["slug"],
-                    "file-name-format": f'devdocs_en_{_get_slug_with_version(item["slug"])}'
+                    "file-name-format": f'devdocs_en_{_get_cleaned(_get_slug_with_version(item["slug"]))}'
                     + "_{period}",
-                    "name-format": f'devdocs_en_{_get_slug_with_version(item["slug"])}',
+                    "name-format": f'devdocs_en_{_get_cleaned(_get_slug_with_version(item["slug"]))}',
                     "description-format": f'{item["name"]} docs by DevDocs',
                     "title-format": f'{item["name"]} Docs',
                     "publisher": "openZIM",
@@ -52,9 +52,7 @@ def get_expected_recipes() -> list[dict[str, Any]]:
                     "memory": 2147483648,
                 },
                 "task_name": "devdocs",
-                # force creation in dev, we are not yet in prod
-                # "warehouse_path": "/devdocs",
-                "warehouse_path": "/.hidden/dev",
+                "warehouse_path": "/devdocs",
             },
             "enabled": True,
             "language": {
@@ -62,16 +60,42 @@ def get_expected_recipes() -> list[dict[str, Any]]:
                 "name_en": "English",
                 "name_native": "English",
             },
-            "name": f'devdocs_en_{_get_slug_with_version(item["slug"])}',
-            # force manually periodicity, we are not yet in prod
-            # "periodicity": "quarterly",
-            "periodicity": "manually",
+            "name": f'devdocs_en_{_get_cleaned(_get_slug_with_version(item["slug"]))}',
+            "periodicity": "quarterly",
             "tags": [
                 "devdocs",
             ],
         }
         for item in group_items_by_type(items)
     ]
+
+
+def _get_icon_url_for_slug(slug: str) -> str:
+    slug_no_version = _get_slug_with_version(slug)
+    if slug_no_version == "moment_timezone":
+        filename = "moment"
+    elif slug_no_version == "vue_router":
+        filename = "vue"
+    elif slug_no_version == "tensorflow_cpp":
+        filename = "tensorflow"
+    elif slug_no_version in ["sanctuary_def", "sanctuary_type_classes"]:
+        filename = "sanctuary"
+    elif slug_no_version in [
+        "minitest",
+        "xslt_xpath",
+        "nginx_lua_module",
+        "liquid",
+        "browser_support_tables",
+        "web_extensions",
+        "tcl_tk",
+        "enzyme",
+        "graphite",
+        "mongoose",
+    ]:
+        filename = "_generic"
+    else:
+        filename = slug_no_version
+    return f"https://drive.farm.openzim.org/devdocs/{filename}.png"
 
 
 def group_items_by_type(items: list[dict[str, Any]]) -> list[dict[str, Any]]:
@@ -104,6 +128,10 @@ def definition_for_name(items: list[dict[str, Any]], name: str) -> dict[str, Any
     )[0]
     print(f'{name}: {variant["slug"]}')
     return {"name": name, "slug": variant["slug"]}
+
+
+def _get_cleaned(value: str) -> str:
+    return re.sub(r"[^.a-zA-Z0-9]", "-", value)
 
 
 def _get_slug_with_version(slug: str) -> str:
