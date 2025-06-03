e486bb6f278f55e3d93cc088306e34f3bdb5bb8e
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-06-03 01:18:17 +0100

    add routes for requesting tasks

--- backend/src/zimfarm_backend/routes/auth/__init__.py
+++ backend/src/zimfarm_backend/routes/auth/__init__.py
@@ -1,9 +1,3 @@
-# class Blueprint(flask.Blueprint):
-#     def __init__(self):
-#         super().__init__("auth", __name__, url_prefix=f"{API_PATH}/auth")
-# methods=["POST"])
-#         self.add_url_rule("/oauth2", "oauth2", OAuth2(), methods=["POST"])
-#
 from zimfarm_backend.routes.auth.logic import router
 
 __all__ = ["router"]
