f7f413589dad47403d35842a91110d1736ee0ac0
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-06-08 03:08:17 +0100

    set up routes for listing platforms

--- backend/src/zimfarm_backend/main.py
+++ backend/src/zimfarm_backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 
-from zimfarm_backend.routes import auth, languages
+from zimfarm_backend.routes import auth, languages, platforms
 
 
 def create_app(*, debug: bool = True):
@@ -11,6 +11,7 @@ def create_app(*, debug: bool = True):
     main_router = APIRouter(prefix="/api/v2")
     main_router.include_router(router=auth.router)
     main_router.include_router(router=languages.router)
+    main_router.include_router(router=platforms.router)
 
     app.include_router(router=main_router)
 
---
+++ backend/src/zimfarm_backend/routes/platforms/logic.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from zimfarm_backend.common.schemas.models import Paginator, Platform
+from zimfarm_backend.routes.models import ListResponse
+
+router = APIRouter(prefix="/platforms", tags=["platforms"])
+
+
+@router.get("/")
+async def get_platforms():
+    """Get a list of supported platforms."""
+    platforms = Platform.all()
+    return ListResponse[Platform](
+        meta=Paginator(
+            skip=0, limit=100, nb_records=len(platforms), page_size=len(platforms)
+        ),
+        items=platforms,
+    )
