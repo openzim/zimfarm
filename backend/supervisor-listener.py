6ba1baab1d89a0c6ec2ef74a3dd176ee5729dc47
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-06-26 04:50:07 +0100

    set up dockerfile and remove old files

--- backend/supervisor-listener.py
+++ backend/supervisor-listener.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
-""" supervisor event listenner for generic script launching
+"""supervisor event listenner for generic script launching
 
-    launches a script (passed as $1+) and communicates with supervisor """
+launches a script (passed as $1+) and communicates with supervisor"""
 
 import datetime
 import pathlib
@@ -12,19 +11,20 @@ import subprocess
 import sys
 
 
-def to_supervisor(text):
+def to_supervisor(text: str):
     # only eventlistener protocol messages may be sent to stdout
     sys.stdout.write(text)
     sys.stdout.flush()
 
 
-def to_log(text):
+def to_log(text: str):
     sys.stderr.write(text)
     sys.stderr.flush()
 
 
-def main(interval, command, args=[]):
-    last_run = None
+def main(interval: int, command: str, args: list[str] | None = None):
+    last_run: datetime.datetime | None = None
+    args = args or []
     while True:
         # transition from ACKNOWLEDGED to READY
         to_supervisor("READY\n")
@@ -36,14 +36,15 @@ def main(interval, command, args=[]):
         headers = dict([x.split(":") for x in line.split()])
         sys.stdin.read(int(headers["len"]))
 
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(datetime.UTC).replace(tzinfo=None)
         if last_run is None or last_run <= now - datetime.timedelta(seconds=interval):
             last_run = now
             script = subprocess.run(
-                [command] + args,
+                [command, *args],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 text=True,
+                check=True,
             )
             to_log(script.stdout)
 
@@ -51,11 +52,11 @@ def main(interval, command, args=[]):
         #     to_supervisor("RESULT 4\nFAIL")
 
         # transition from READY to ACKNOWLEDGED
-        to_supervisor("RESULT 2\nOK")
+        to_supervisor("RESULT \nOK")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 3:  # noqa: PLR2004
         to_log("missing interval and/or script, crashing")
         sys.exit(1)
 
