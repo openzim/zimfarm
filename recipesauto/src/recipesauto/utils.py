93235d0f3aa93f04c0289ba4a129ec7b89281b90
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-09-01 12:40:16 +0100

    Add gutenberg recipes management

--- recipesauto/src/recipesauto/utils.py
+++ recipesauto/src/recipesauto/utils.py
@@ -1,5 +1,9 @@
 import re
 
+import pycountry
+
+from recipesauto.constants import logger
+
 name_pattern = re.compile(r"^[a-z0-9\.\-]*_[a-z0-9\.\-]*_[a-z0-9\.\-]*_?[a-z0-9\.\-]*$")
 
 
@@ -7,3 +11,16 @@ def check_zim_name(zim_name: str):
     if not name_pattern.match(zim_name):
         raise Exception(f"Bad zim name: {zim_name}")
     return zim_name
+
+
+def get_iso_639_3_code(code: str) -> str | None:
+    """Get ISO 639-3 language code for a given code"""
+    language = pycountry.languages.get(alpha_3=code)
+    # If not found, attempt to get the language code from the ISO 639-1 code
+    if not language:
+        language = pycountry.languages.get(alpha_2=code)
+
+    if not language:
+        logger.warn(f"Could not find ISO 639-3 language code for '{code}'")
+        return None
+    return language.alpha_3
