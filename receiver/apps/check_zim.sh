0f39a8b6752958b3786fb5d8531ebd85d88ca831
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-09-04 17:05:59 +0100

    remove zimcheck from receiver

--- receiver/apps/check_zim.sh
+++ receiver/apps/check_zim.sh
@@ -2,69 +2,35 @@
 #
 # Author : Florent Kaisser
 #
-# Usage : check_zim.sh <zimFilePath> <zimSrcDir> <zimDstDir> <zimQuarantineDir> <logDir> <zimCheckOptions> [NO_QUARANTINE|NO_CHECK]
+# Usage : check_zim.sh <zimFilePath> <zimSrcDir> <zimDstDir> <zimQuarantineDir>
 #
-
-ZIMCHECK=/usr/local/bin/zimcheck
 
 ZIMFILE=$1
 ZIMSRCDIR=$2
-ZIMCHECK_OPTION=$6
-OPTION=$7
 
 
 ZIMPATH=$(echo $ZIMFILE | sed "s:$ZIMSRCDIR::")
 
 DESTFILE=$3$ZIMPATH
-DESTDIR=$(dirname $DESTFILE)
+DESTDIR=$(dirname "$DESTFILE")
 
-if [ "$OPTION" = "NO_QUARANTINE" ]
-then
- QUARDIR=$DESTDIR
- QUARFILE=$DESTFILE
-else
- QUARFILE=$4$ZIMPATH
- QUARDIR=$(dirname $QUARFILE)
-fi
+QUARFILE=$4$ZIMPATH
+QUARDIR=$(dirname "$QUARFILE")
 
-LOGFILE=$(echo "$5$ZIMPATH" | cut -f 1 -d '.').log
-LOGDIR=$(dirname $5$ZIMPATH)
-
-NAME_FORMAT="^(.+?_)([a-z\-]{2,10}?_|)(.+_|)([\d]{4}|)\-([\d]{2})$"
-ZIM_NAME=$(basename "$ZIMPATH" .zim)
 
 function moveZim () {
-   mkdir -p $1
-   mv -f $ZIMFILE $2
+   mkdir -p "$1"
+   mv -f "$ZIMFILE" "$2"
 }
 
 # prevent zim from being uploaded to root of zimDstDir
 if [ $(dirname $ZIMPATH) = "." ] ; then
   echo "Zim outside subfolder. moving to quarantine"
   QUARFILE=$4$ZIMPATH
-  QUARDIR=$(dirname $QUARFILE)
-  moveZim $QUARDIR $QUARFILE
+  QUARDIR=$(dirname "$QUARFILE")
+  moveZim "$QUARDIR" "$QUARFILE"
   exit 0
 fi
 
-if [ "$OPTION" = "NO_CHECK" ]
-then
-  echo "move $ZIMFILE to $DESTFILE"
-  moveZim $DESTDIR $DESTFILE
-else
-  mkdir -p $LOGDIR
-  if $ZIMCHECK $ZIMCHECK_OPTION $ZIMFILE > $LOGFILE 2>&1
-  then
-   if (echo $ZIM_NAME | grep -q -P $NAME_FORMAT)
-   then
-    echo "$ZIMFILE is valid" >> $LOGFILE
-    moveZim $DESTDIR $DESTFILE
-   else
-    echo "$ZIMFILE is valid but name is in an invalid format" >> $LOGFILE
-    moveZim $QUARDIR $QUARFILE
-   fi
-  else
-   echo "$ZIMFILE is not valid" >> $LOGFILE
-   moveZim $QUARDIR $QUARFILE
-  fi
-fi
+echo "move $ZIMFILE to $DESTFILE"
+moveZim "$DESTDIR" "$DESTFILE"
