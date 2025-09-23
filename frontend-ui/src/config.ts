998a86b21ea03106d8c5e3dc2124138cc1a87354
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-09-23 10:33:57 +0100

    enable site stats tracking with matomo

--- frontend-ui/src/config.ts
+++ frontend-ui/src/config.ts
@@ -6,6 +6,10 @@ import { inject } from 'vue'
 export interface Config {
   ZIMFARM_WEBAPI: string
   ZIMFARM_ZIM_DOWNLOAD_URL: string
+  MATOMO_ENABLED: boolean
+  MATOMO_HOST: string
+  MATOMO_SITE_ID: number
+  MATOMO_TRACKER_FILE_NAME: string
 }
 
 export const ConfigService = {
