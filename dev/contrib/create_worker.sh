55fda9da2351e3545ebbb7e188e39f3698a80306
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-10-07 05:01:44 +0100

    update create worker contrib script

--- dev/contrib/create_worker.sh
+++ dev/contrib/create_worker.sh
@@ -4,7 +4,7 @@
 # - retrieve an admin token
 # - create the `test_worker`` user
 # - create the associated worker object
-# - upload a test public key. 
+# - upload a test public key.
 #
 # To be used to have a "real" test worker for local development, typically to start
 # a worker manager or a task manager or simply assign tasks to a worker in the UI/API
@@ -14,16 +14,16 @@ set -e
 echo "Retrieving admin access token"
 
 ZF_ADMIN_TOKEN="$(curl -s -X 'POST' \
-    'http://localhost:8000/v1/auth/authorize' \
+    'http://localhost:8000/v2/auth/authorize' \
     -H 'accept: application/json' \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -d 'username=admin&password=admin' \
+    -H 'Content-Type: application/json' \
+    -d '{"username": "admin", "password": "admin"}' \
     | jq -r '.access_token')"
 
 echo "Create test_worker user"
 
 curl -s -X 'POST' \
-  'http://localhost:8000/v1/users/' \
+  'http://localhost:8000/v2/users' \
   -H 'accept: */*' \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $ZF_ADMIN_TOKEN" \
@@ -37,16 +37,16 @@ curl -s -X 'POST' \
 echo "Retrieving test_worker access token"
 
 ZF_USER_TOKEN="$(curl -s -X 'POST' \
-    'http://localhost:8000/v1/auth/authorize' \
+    'http://localhost:8000/v2/auth/authorize' \
     -H 'accept: application/json' \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    -d 'username=test_worker&password=test_worker' \
+    -H 'Content-Type: application/json' \
+    -d '{"username": "test_worker", "password": "test_worker"}' \
     | jq -r '.access_token')"
 
 echo "Worker check-in (will create it since missing)"
 
 curl -s -X 'PUT' \
-  'http://localhost:8000/v1/workers/test_worker/check-in' \
+  'http://localhost:8000/v2/workers/test_worker/check-in' \
   -H 'accept: */*' \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $ZF_USER_TOKEN" \
@@ -56,16 +56,26 @@ curl -s -X 'PUT' \
   "memory": 1024,
   "disk": 0,
   "offliners": [
-    "zimit"
+    "zimit",
+    "ted",
+    "phet",
+    "mwoffliner",
+    "openedx",
+    "sotoki",
+    "freecodecamp",
+    "ifixit"
   ]
 }'
 
-echo "Add private key to test_worker"
+echo "Generating SSH key pair (Ed25519)"
+ssh-keygen -t ed25519 -f id_ed25519 -N ""
+payload="$(jq -n --arg key "$(< id_ed25519.pub)" '{key: $key}')"
 
-curl -X POST http://localhost:8000/v1/users/test_worker/keys \
+
+curl -X POST http://localhost:8000/v2/users/test_worker/keys \
   -H 'accept: */*' \
   -H "Authorization: Bearer $ZF_USER_TOKEN" \
   -H 'Content-Type: application/json; charset=utf-8' \
-  -d '{"name": "test_key", "key": "AAAAB3NzaC1yc2EAAAADAQABAAABAQCn2r5IZSJp02FBAYSZBQRdOBKBK2VOErdrBCZm5Ig3hDKQuxq38+W5CJ2JUJU+LQm//uenm58scGlEtk5+w5SjObjzK8Qx6JeRhAiZ8xpyydSoUIvd0ARD9OKwdiQFqVlLPlOyrdIpQ2vRESdwzhe0f7EYUwgKzBw5k0foxQsGxTiztY/ugWJ8Jso5WOxXwzEw4cSnGhdrehqLphlZanr54wj5oTcrj/vJHlpbxkYzFMc2Zgj81GdIV4yP3H1yX4ySK8VkDPOCczHacdRnHw4u8Vgf6wS6Zy3iMpvuGu7BJkwNoTXvmVV5BXUm6GAMSQTAPcw5T8M+eXjSAnriGDAL"}'
+  -d "$payload"
 
 echo "DONE"
