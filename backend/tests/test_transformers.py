786db61056b10026908c783838e057d284faa357
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-10-13 10:37:59 +0100

    tests for similarity generator

--- backend/tests/test_transformers.py
+++ backend/tests/test_transformers.py
@@ -1,7 +1,19 @@
-import pytest
+# ruff: noqa: E501
+from contextlib import nullcontext as does_not_raise
+from typing import Any
 
-from zimfarm_backend.common.schemas.offliners.models import TransformerSchema
+import pytest
+from _pytest.python_api import RaisesContext
+
+from zimfarm_backend.common.enums import DockerImageName
+from zimfarm_backend.common.schemas.offliners.builder import generate_similarity_data
+from zimfarm_backend.common.schemas.offliners.models import (
+    OfflinerSpecSchema,
+    SimilarityDataSchema,
+    TransformerSchema,
+)
 from zimfarm_backend.common.schemas.offliners.transformers import transform_data
+from zimfarm_backend.common.schemas.orms import OfflinerSchema
 
 
 @pytest.mark.parametrize(
@@ -44,3 +56,192 @@ def test_transform_data(
     transformers: list[TransformerSchema], data: list[str], expected: list[str]
 ):
     assert transform_data(data, transformers) == expected
+
+
+@pytest.mark.parametrize(
+    "similarity_data,data,expected_similarity_data",
+    [
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seeds", transformers=[TransformerSchema(name="hostname")]
+                )
+            ],
+            {
+                "seeds": "https://www.mankier.com/stats",
+            },
+            ["www.mankier.com"],
+            id="transform-seed-hostname",
+        ),
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seed_file",
+                    transformers=[
+                        TransformerSchema(name="split", operator=","),
+                        TransformerSchema(name="hostname"),
+                    ],
+                )
+            ],
+            {
+                "seedFile": "https://www.mankier.com/stats,https://dart.dev/tutorials",
+            },
+            ["www.mankier.com", "dart.dev"],
+            id="transform-seed-file-by-alias",
+        ),
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seed_file",
+                    transformers=[
+                        TransformerSchema(name="split", operator=","),
+                        TransformerSchema(name="hostname"),
+                    ],
+                )
+            ],
+            {
+                "seedFile": "https://www.mankier.com/stats,https://dart.dev/tutorials",
+            },
+            ["www.mankier.com", "dart.dev"],
+            id="transform-seed-file-by-flag-name",
+        ),
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seed_file",
+                    transformers=[
+                        TransformerSchema(name="split", operator=","),
+                        TransformerSchema(name="hostname"),
+                    ],
+                ),
+                SimilarityDataSchema(
+                    flag="seeds",
+                    transformers=[
+                        TransformerSchema(name="hostname"),
+                    ],
+                ),
+            ],
+            {
+                "seeds": "https://www.mankier.com/stats",
+                "seedFile": "https://dart.dev/tutorials",
+            },
+            ["www.mankier.com", "dart.dev"],
+            id="transform-seed-file-by-flag-name-and-seeds",
+        ),
+    ],
+)
+def test_generate_similarity_data(
+    similarity_data: list[SimilarityDataSchema],
+    data: dict[str, Any],
+    expected_similarity_data: str,
+):
+    spec = OfflinerSpecSchema.model_validate_json(
+        """{
+          "offliner_id": "zimit",
+          "stdOutput": true,
+          "stdStats": "zimit-progress-file",
+          "flags": {
+            "seeds": {
+              "type": "string",
+              "required": false,
+              "title": "Seeds",
+              "description": "The seed URL(s) to start crawling from. Multile seed URL must be separated by a comma (usually not needed, these are just the crawl seeds). First seed URL is used as ZIM homepage"
+            },
+            "seed_file": {
+              "type": "string",
+              "required": false,
+              "title": "Seed File",
+              "description": "If set, read a list of seed urls, one per line. HTTPS URL to an online file."
+            }
+          }
+        }""",
+    )
+    spec.similarity_data = similarity_data
+    offliner = OfflinerSchema(
+        id="zimit",
+        base_model="CamelModel",
+        docker_image_name=DockerImageName.zimit,
+        command_name="zimit2zim",
+        ci_secret_hash=None,
+    )
+    results = generate_similarity_data(data, offliner, spec)
+    assert len(results) == len(expected_similarity_data)
+    for x in results:
+        assert x in expected_similarity_data
+
+
+@pytest.mark.parametrize(
+    "similarity_data,data,expected",
+    [
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seeds", transformers=[TransformerSchema(name="hostname")]
+                )
+            ],
+            {
+                "seeds": "https://www.mankier.com/stats",
+            },
+            does_not_raise(),
+            id="transform-seed-hostname",
+        ),
+        pytest.param(
+            [
+                SimilarityDataSchema(
+                    flag="seed_file",
+                    transformers=[
+                        TransformerSchema(name="split", operator=","),
+                        TransformerSchema(name="hostname"),
+                    ],
+                ),
+                SimilarityDataSchema(
+                    flag="seeds",
+                    transformers=[
+                        TransformerSchema(name="hostname"),
+                    ],
+                ),
+            ],
+            {
+                "seedFile": "https://dart.dev/tutorials",
+            },
+            pytest.raises(ValueError),
+            id="transform-seed-file-but-missing-seeds-data",
+        ),
+    ],
+)
+def test_generate_similarity_data_value_missing(
+    similarity_data: list[SimilarityDataSchema],
+    data: dict[str, Any],
+    expected: RaisesContext[Exception],
+):
+    spec = OfflinerSpecSchema.model_validate_json(
+        """{
+          "offliner_id": "zimit",
+          "stdOutput": true,
+          "stdStats": "zimit-progress-file",
+          "flags": {
+            "seeds": {
+              "type": "string",
+              "required": false,
+              "title": "Seeds",
+              "description": "The seed URL(s) to start crawling from. Multile seed URL must be separated by a comma (usually not needed, these are just the crawl seeds). First seed URL is used as ZIM homepage"
+            },
+            "seed_file": {
+              "type": "string",
+              "required": false,
+              "title": "Seed File",
+              "description": "If set, read a list of seed urls, one per line. HTTPS URL to an online file."
+            }
+          }
+        }""",
+    )
+    spec.similarity_data = similarity_data
+    offliner = OfflinerSchema(
+        id="zimit",
+        base_model="CamelModel",
+        docker_image_name=DockerImageName.zimit,
+        command_name="zimit2zim",
+        ci_secret_hash=None,
+    )
+    with expected:
+        generate_similarity_data(data, offliner, spec)
