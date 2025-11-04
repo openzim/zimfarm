b3793f41781af5db23f7b104e483ba70bd9471dd
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-11-04 14:42:19 +0100

    upgrade monitor

--- frontend-ui/src/views/TaskDetailView.vue
+++ frontend-ui/src/views/TaskDetailView.vue
@@ -538,7 +538,7 @@ const offsetString = computed(() => {
 })
 
 const shortId = computed(() => {
-  return props.id.substring(0, 8)
+  return props.id.substring(0, 5)
 })
 
 const isRunning = computed(() => {
@@ -652,7 +652,7 @@ const hasStats = computed(() => {
 })
 
 const monitoringUrl = computed(() => {
-  return `http://monitoring.openzim.org/host/${scheduleName.value}_${shortId.value}.${
+  return `${config.MONITORING_URL}/host/${scheduleName.value}_${shortId.value}.${
     task.value?.worker_name
   }/#menu_cgroup_zimscraper_${task.value?.original_schedule_name}_${
     shortId.value
@@ -662,7 +662,6 @@ const monitoringUrl = computed(() => {
     getTimestampStringForStatus(task.value?.timestamp, 'scraper_completed', '') || 0,
   ).getTime()};theme=slate;utc=Africa/Bamako`
 })
-
 const webApiUrl = computed(() => config.ZIMFARM_WEBAPI)
 const kiwixDownloadUrl = computed(() => config.ZIMFARM_ZIM_DOWNLOAD_URL)
 const zimfarmLogsUrl = computed(() => (task.value ? logsUrl(task.value) : ''))
