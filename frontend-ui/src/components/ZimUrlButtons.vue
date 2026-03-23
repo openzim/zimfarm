8b772fffc48e5da97c1500c49d1aed8a8a400334
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2026-03-23 15:56:14 +0100

    show files as cards

--- frontend-ui/src/components/ZimUrlButtons.vue
+++ frontend-ui/src/components/ZimUrlButtons.vue
@@ -1,33 +1,24 @@
 <template>
   <div v-if="loading" class="d-flex align-center">
     <v-progress-circular indeterminate size="20" width="2" />
-    <span v-if="!compact && !iconOnly" class="ml-2 text-grey">Loading URLs...</span>
   </div>
-  <div v-else-if="urls && urls.length > 0" :class="iconOnly ? 'd-inline-flex align-center' : ''">
+  <div v-else-if="urls && urls.length > 0" class="d-inline-flex align-center">
     <v-tooltip v-for="url in urls" :key="`${url.kind}-${url.collection}`" location="bottom">
       <template #activator="{ props: tooltipProps }">
         <v-btn
           v-bind="tooltipProps"
           :href="url.url"
           target="_blank"
-          :icon="iconOnly || compact"
-          :prepend-icon="
-            iconOnly || compact ? undefined : url.kind === 'download' ? 'mdi-download' : 'mdi-eye'
-          "
-          :variant="iconOnly ? 'text' : compact ? 'text' : 'outlined'"
-          :size="iconOnly ? 'x-small' : compact ? 'x-small' : 'small'"
-          :class="iconOnly ? 'ml-1' : compact ? '' : 'mr-2'"
+          icon
+          variant="text"
+          :size="compact ? 'x-small' : 'small'"
         >
-          <v-icon v-if="iconOnly || compact" :size="iconOnly ? 'small' : undefined">
-            {{ url.kind === 'download' ? 'mdi-download' : 'mdi-eye' }}
-          </v-icon>
-          <span v-else>{{ url.kind === 'download' ? 'Download' : 'View' }}</span>
+          <v-icon>{{ url.kind === 'download' ? 'mdi-download' : 'mdi-eye' }}</v-icon>
         </v-btn>
       </template>
       <span>{{ url.kind === 'download' ? 'Download' : 'View' }} ({{ url.collection }})</span>
     </v-tooltip>
   </div>
-  <span v-else-if="!iconOnly" class="text-grey">{{ emptyText }}</span>
 </template>
 
 <script setup lang="ts">
@@ -38,15 +29,11 @@ withDefaults(
     urls?: ZimUrl[]
     loading?: boolean
     compact?: boolean
-    iconOnly?: boolean
-    emptyText?: string
   }>(),
   {
     urls: undefined,
     loading: false,
     compact: false,
-    iconOnly: false,
-    emptyText: 'No URLs available',
   },
 )
 </script>
